e3150df86ef698f7e4538d7d54cf0bc2486e656b
Farhad rasouli <rasouli.farhad@gmail.com>
2023-05-28 03:26:52 +0330

    updated

--- java-collection-demo/src/main/java/com/farhad/example/collections/random/RandomGenerator.java
+++ java-collection-demo/src/main/java/com/farhad/example/collections/random/RandomGenerator.java
@@ -2,15 +2,16 @@ package com.farhad.example.collections.random;
 
 public class RandomGenerator {
     
-    public static double rndD() {
+    public static double rnd() {
         return Math.random();
     }
 
-    public static double rndD(int min, int max) {
-       return ( Math.random() *  ( ( max - min ) + 1 )  + min );
-    }
+    public static double rnd(double min, double max) {
+        return ( Math.random() *  ( ( max - min ) + 1 )  + min );
+     }
+
     public static int rndI(int min, int max) {
-        return (int) Math.floor(rndD(min, max));
+        return (int) Math.floor(rnd(min, max));
      }
  
 }
