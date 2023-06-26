43377339ca8d00b13bae2aa4e38098cf16622168
Farhad rasouli <rasouli.farhad@gmail.com>
2023-06-26 19:24:46 +0330

    updated

--- guava-demo/src/main/java/com/farhad/example/guava/GuavaCoreDemo.java
+++ guava-demo/src/main/java/com/farhad/example/guava/GuavaCoreDemo.java
@@ -97,7 +97,9 @@ public class GuavaCoreDemo {
         private static final String UNKNOWN_COORDINATES = "Unknown coordinates";
 
         public String getCoordinatesAsText() {
-            return getGpsCoordinates() != null ? getGpsCoordinates() : UNKNOWN_COORDINATES; 
+            // return getGpsCoordinates() != null ? getGpsCoordinates() : UNKNOWN_COORDINATES; 
+            String gpsCoordinates = getGpsCoordinates();
+            return gpsCoordinates != null ? gpsCoordinates : UNKNOWN_COORDINATES;
         }
 
         public String getGpsCoordinates() {
