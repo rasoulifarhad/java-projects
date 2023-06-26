bf32773495f27f0578135139b36d6973ddc98ab6
Farhad rasouli <rasouli.farhad@gmail.com>
2023-06-26 09:38:20 +0330

    updated

--- guava-demo/src/main/java/com/farhad/example/guava/model/Money.java
+++ guava-demo/src/main/java/com/farhad/example/guava/model/Money.java
@@ -2,6 +2,8 @@ package com.farhad.example.guava.model;
 
 import java.math.BigDecimal;
 
+import com.google.common.base.Preconditions;
+
 public class Money {
     private final BigDecimal amount;
     private final Currency currency;
@@ -11,15 +13,18 @@ public class Money {
     }
 
     public Money(BigDecimal amount, Currency currency) {
-        if (amount == null) {
-            throw new NullPointerException("amount must not be null");
-        }
-        if(currency == null) {
-            throw new NullPointerException("currency must not be null");
-        }
-        if (amount.signum() < 0 ) {
-            throw new IllegalArgumentException("amount must be positive " + amount);
-        }
+        // if (amount == null) {
+        //     throw new NullPointerException("amount must not be null");
+        // }
+        // if(currency == null) {
+        //     throw new NullPointerException("currency must not be null");
+        // }
+        // if (amount.signum() < 0 ) {
+        //     throw new IllegalArgumentException("amount must be positive " + amount);
+        // }
+        Preconditions.checkNotNull(amount, "amount must not be null");
+        Preconditions.checkNotNull(currency, "currency must not be null");
+        Preconditions.checkArgument(amount.signum() >= 0, "amount must be positive: %s", amount);
         this.amount = amount;
         this.currency = currency;
     }
