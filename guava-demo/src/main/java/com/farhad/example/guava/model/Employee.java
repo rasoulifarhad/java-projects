9ca69e13fe708756cb363704b3befc8d2a20c320
Farhad rasouli <rasouli.farhad@gmail.com>
2023-06-26 05:42:01 +0330

    updated

--- guava-demo/src/main/java/com/farhad/example/guava/model/Employee.java
+++ guava-demo/src/main/java/com/farhad/example/guava/model/Employee.java
@@ -1,5 +1,7 @@
 package com.farhad.example.guava.model;
 
+import com.google.common.base.Objects;
+
 import lombok.Data;
 
 @Data
@@ -13,15 +15,29 @@ public class Employee {
             return false;
         }
         Employee that = (Employee)object;
-        return (name == that.getName() || (name != null && name.equals(that.getName()) ))
-                && (age == that.getAge() || (age != null && age.equals(that.getAge())))
-                && job == that.getJob();
+        // return (name == that.getName() || (name != null && name.equals(that.getName()) ))
+        //         && (age == that.getAge() || (age != null && age.equals(that.getAge())))
+        //         && job == that.getJob();
+        return Objects.equal(name, that.getName())
+            && Objects.equal(age, that.getAge())
+            && job == that.getJob();
     }
 
     public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (age != null ? age.hashCode() : 0);
-        result = 31 * result + (job != null ? job.hashCode() : 0);
-        return result;
+        // int result = name != null ? name.hashCode() : 0;
+        // result = 31 * result + (age != null ? age.hashCode() : 0);
+        // result = 31 * result + (job != null ? job.hashCode() : 0);
+        // return result;
+        return Objects.hashCode(name, age, job);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Employee.class.getSimpleName());
+        sb.append("{name=").append(name);
+        sb.append(", age=").append(age);
+        sb.append(", job=").append(job);
+        sb.append("}");
+        return sb.toString();
     }
 }
