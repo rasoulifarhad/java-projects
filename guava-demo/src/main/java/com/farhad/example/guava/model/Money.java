6dd1be85cf9ac603708c6ab7a2843580a98ccf06
Farhad rasouli <rasouli.farhad@gmail.com>
2023-06-26 09:41:20 +0330

    updated

--- guava-demo/src/main/java/com/farhad/example/guava/model/Money.java
+++ guava-demo/src/main/java/com/farhad/example/guava/model/Money.java
@@ -22,10 +22,13 @@ public class Money {
         // if (amount.signum() < 0 ) {
         //     throw new IllegalArgumentException("amount must be positive " + amount);
         // }
-        Preconditions.checkNotNull(amount, "amount must not be null");
-        Preconditions.checkNotNull(currency, "currency must not be null");
+        // Preconditions.checkNotNull(amount, "amount must not be null");
+        // Preconditions.checkNotNull(currency, "currency must not be null");
+        // Preconditions.checkArgument(amount.signum() >= 0, "amount must be positive: %s", amount);
+        // this.amount = amount;
+        // this.currency = currency;
+        this.amount = Preconditions.checkNotNull(amount, "amount must not be null");
+        this.currency = Preconditions.checkNotNull(currency, "currency must not be null");
         Preconditions.checkArgument(amount.signum() >= 0, "amount must be positive: %s", amount);
-        this.amount = amount;
-        this.currency = currency;
     }
 }
