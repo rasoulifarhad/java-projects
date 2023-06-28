75897995d372fa768c87fd959f8ab2d405f134aa
Farhad rasouli <rasouli.farhad@gmail.com>
2023-06-28 18:35:07 +0330

    updated

--- java-io-project/src/main/java/com/farhad/example/io/SmallFilesDemo.java
+++ java-io-project/src/main/java/com/farhad/example/io/SmallFilesDemo.java
@@ -1,5 +1,76 @@
 package com.farhad.example.io;
 
+import static java.nio.file.StandardOpenOption.READ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.util.EnumSet;
+import java.util.Set;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * The OpenOptions Parameter
+ *
+ * Several of the methods in this section take an optional OpenOptions parameter. This parameter is optional and the API tells 
+ * you what the default behavior is for the method when none is specified.
+ * 
+ * The following StandardOpenOptions enums are supported:
+ * 
+ * - WRITE – Opens the file for write access.
+ * - APPEND – Appends the new data to the end of the file. This option is used with the WRITE or CREATE options.
+ * - TRUNCATE_EXISTING – Truncates the file to zero bytes. This option is used with the WRITE option.
+ * - CREATE_NEW – Creates a new file and throws an exception if the file already exists.
+ * - CREATE – Opens the file if it exists or creates a new file if it does not.
+ * - DELETE_ON_CLOSE – Deletes the file when the stream is closed. This option is useful for temporary files.
+ * - SPARSE – Hints that a newly created file will be sparse. This advanced option is honored on some file systems, such as NTFS, where large files with data "gaps" can be stored in a more efficient manner where those empty gaps do not consume disk space.
+ * - SYNC – Keeps the file (both content and metadata) synchronized with the underlying storage device.
+ * - DSYNC – Keeps the file content synchronized with the underlying storage device.
+ * 
+ */
+
+ @Slf4j
 public class SmallFilesDemo {
-    
+
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+
+    public static void readFile(Path path) throws IOException {
+        readFile(path, EnumSet.of(READ));
+    }
+
+    public static void readFile(Path path, Set<? extends OpenOption> options) throws IOException {
+        try(ReadableByteChannel rbc = Files.newByteChannel(path, options)) {
+            final int BUFFER_CAPACITY = 10;
+            ByteBuffer buf = ByteBuffer.allocate(BUFFER_CAPACITY);
+            String encoding = System.getProperty("file.encoding");
+            while(rbc.read(buf) > 0 ) {
+                // Flips this buffer. The limit is set to the current position and then the position is set to zero.
+                // After a sequence of channel-read or put operations, invoke this method to prepare for a sequence of channel-write or relative get operations.
+                // buf.put(magic);    // Prepend header
+                // in.read(buf);      // Read data into rest of buffer
+                // buf.flip();        // Flip buffer
+                // out.write(buf);    // Write header + data to channe
+                buf.flip();
+                System.out.print(Charset.forName(encoding).decode(buf));
+                buf.clear();  
+            }
+        }
+    }
+
+    public static void readAndLogAllLines(Path file) throws IOException {
+        try(BufferedReader reader = Files.newBufferedReader(file, UTF8)) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } 
+    }
+
 }
+
--- java-io-project/src/test/java/com/farhad/example/io/SmallFilesDemoTest.java
+++ java-io-project/src/test/java/com/farhad/example/io/SmallFilesDemoTest.java
@@ -1,19 +1,37 @@
 package com.farhad.example.io;
 
+import static com.farhad.example.io.SmallFilesDemo.readAndLogAllLines;
+import static com.farhad.example.io.SmallFilesDemo.readFile;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URI;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.charset.Charset;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
@@ -79,7 +97,7 @@ public class SmallFilesDemoTest {
     }
 
     @Test
-    public void demonstrateWritingAFileUsingBufferedWriter() {
+    public void demonstrateWritingAFileUsingBufferedWriter() throws IOException {
         URL url = this.getClass().getClassLoader().getResource("smallFile.txt");
         Path file = Paths.get(url.getFile());
         Path parent = file.getParent();
@@ -113,15 +131,197 @@ public class SmallFilesDemoTest {
         }
     }
 
-
-    private void readAndLogAllLines(Path file) {
-        try(BufferedReader reader = Files.newBufferedReader(file, UTF8)) {
-            String line;
-            System.out.println(file.getFileName() + " :");
-            while( (line = reader.readLine()) != null ) {
-                System.out.println(line);
-            }
+    @Test
+    public void demonstrateWritingFilesUsingStreamIO() throws IOException {
+        String s = "Hello world!";
+        byte [] data = s.getBytes();
+        Path logFile = Paths.get("./logfile.txt");
+        try(OutputStream out = new BufferedOutputStream(Files.newOutputStream(logFile,CREATE, APPEND))) {
+            out.write(data, 0, data.length);
         } catch (IOException e) {
+            log.error("IOException:", e);
+        }
+        assertTrue(Files.exists(logFile));
+        readAndLogAllLines(logFile);
+    }
+
+    @Test
+    public void demonstrateWritingFilesUsingStreamIODeleteFirst() throws IOException {
+        String s = "Hello world!";
+        byte [] data = s.getBytes();
+        Path logFile = Paths.get("./logfile.txt");
+        deleteFile(logFile);
+        try(OutputStream out = new BufferedOutputStream(Files.newOutputStream(logFile,CREATE, APPEND))) {
+            out.write(data, 0, data.length);
+        } catch (IOException e) {
+            log.error("IOException:", e);
+        }
+        assertTrue(Files.exists(logFile));
+        readAndLogAllLines(logFile);
+    }
+
+    // While stream I/O reads a character at a time, channel I/O reads a buffer at a time. 
+    @Test
+    public void demonstrateReadingAndWritingFilesUsingChannelIO() {
+
+    }
+
+    @Test
+    public void demonstrateCreatesFileOrAppendToFileAlreadyExistWithSpecificSetOfFilePermissionsForPosixSystems() throws IOException {
+        Set<OpenOption> options = new HashSet<>();
+        options.add(APPEND);
+        options.add(CREATE);
+
+        Set<PosixFilePermission> perms = 
+            PosixFilePermissions.fromString("rw-r-----");
+        FileAttribute<Set<PosixFilePermission>> attr = PosixFilePermissions.asFileAttribute(perms);
+
+        // convert string to bytBuffer
+        String s = "Hello World! \n";
+        byte [] data = s.getBytes();
+        ByteBuffer bb = ByteBuffer.wrap(data);
+
+        Path file = Paths.get("./fileWithPermission.txt");
+
+        deleteFile(file);
+        assertFalse(Files.exists(file));
+
+        try(SeekableByteChannel sbc = Files.newByteChannel(file, options, attr)) {
+            sbc.write(bb);
+        } catch (IOException e) {
+            log.error("IOException thrown:", e);
+        }
+
+        assertTrue(Files.exists(file));
+        readFile(file);
+        deleteFile(file);
+
+    }
+
+    @Test
+    public void demonstrateCreateFiles() {
+        Path file = Paths.get("./createFileExample.txt");
+        try {
+            Files.createFile(file);
+            Files.createFile(file);
+        } catch (FileAlreadyExistsException e) {
+            log.error("File already exists: {}", file, e);
+        } catch (IOException e) {
+            log.error("create file error: ", e);
+        }
+
+        assertTrue(Files.exists(file));
+
+        deleteFile(file);
+
+        assertFalse(Files.exists(file));
+    }
+
+    @Test
+    public void demonstrateCreateFilesWithOutputStreamIO() throws IOException {
+        Path path = Paths.get("./createFileExample.txt");
+        deleteFile(path);
+        OutputStream out = Files.newOutputStream(path);
+        out.close();
+
+        assertTrue(Files.exists(path));
+        deleteFile(path);
+        assertFalse(Files.exists(path));
+    }
+
+    @Test
+    public void demonstrateCreateTemporaryFiles() {
+        try {
+            Path path = Files.createTempFile(null, ".test");
+            log.info(" temp file with name {} created", path);
+            deleteFile(path);
+            assertFalse(Files.exists(path));
+        } catch(IOException e) {
+            log.error("IOException", e) ;
+        }   
+    }
+
+    @Test
+    public void demonstrateCreatePaths() {
+        Path p1 = Paths.get("/tmp/tmpFile.txt");
+        Path p2 = Paths.get(URI.create("file:///tmp/tmpFile.txt"));
+        assertTrue(p1.equals(p2));
+
+        Path p3 = FileSystems.getDefault().getPath("/tmp/tmpFile.txt");
+        assertTrue(p2.equals(p3));
+
+        String uHome = System.getProperty("user.home");
+        Path p4 = Paths.get(uHome, "logs", "foo.log");
+        log.info("Path: {}", p4);
+    }
+
+    // Think `Path` as storing these name elements as a sequence. The highest element in the directory structure would be located at index 0. The 
+    // lowest element in the directory structure would be located at index [n-1], where n is the number of name elements in the Path. 
+    @Test
+    public void demonstrateRetrievingInformationFromPath() {
+        Path uPath = Paths.get("/home/farhad/foo/bar");
+        log.info("To string: {}", uPath.toString());
+        log.info("File name: {}", uPath.getFileName());
+        log.info("Name at 0: {}", uPath.getName(0));
+        log.info("Name count: {}", uPath.getNameCount());
+        log.info("Parent: {}", uPath.getParent());
+        log.info("Root: {}", uPath.getRoot());
+        log.info("sub path(0,2): {}", uPath.subpath(0,2));
+
+        log.info("");
+
+        Path wPath = Paths.get("C:\\home\\farhad\\foo\\bar");
+        log.info("To string: {}", wPath.toString());
+        log.info("File name: {}", wPath.getFileName());
+        log.info("Name at 0: {}", wPath.getName(0));
+        log.info("Name count: {}", wPath.getNameCount());
+        log.info("Parent: {}", wPath.getParent());
+        log.info("Root: {}", wPath.getRoot());
+        log.info("sub path(0,1): {}", wPath.subpath(0,1));
+
+        log.info("");
+        uPath = Paths.get("foo/bar");
+        log.info("To string: {}", uPath.toString());
+        log.info("File name: {}", uPath.getFileName());
+        log.info("Name at 0: {}", uPath.getName(0));
+        log.info("Name count: {}", uPath.getNameCount());
+        log.info("Parent: {}", uPath.getParent());
+        log.info("Root: {}", uPath.getRoot());
+        log.info("sub path(0,2): {}", uPath.subpath(0,2));
+    }
+
+    @Test
+    public void demonstrateNormalizePath() {
+        Path path = Paths.get("/home/./foo/bar") ;
+        Path normalizedPath = path.normalize();
+        log.info("Normalized path: {}", normalizedPath);
+
+        path = Paths.get("/home/farhad/../foo/bar");
+        log.info("Normalized path: {}", normalizedPath);
+    }
+
+    // You can use three methods to convert the Path. 
+    // 
+    // If you need to convert the path to a string that can be opened from a browser, you can use toUri.
+    // 
+    // The toAbsolutePath method converts a path to an absolute path.
+    // 
+    // The toRealPath method returns the real path of an existing file.
+    @Test
+    public void demonstrateConvertingPath() {
+        Path pathToUri = Paths.get("/home/farhad/foo/bar.txt");
+        log.info("path: {}, Path to URI: {}", pathToUri, pathToUri.toUri());
+
+        Path pathToAbsolute = Paths.get("bar.txt");
+        log.info("path: {}, Path to absolute: {}", pathToAbsolute, pathToAbsolute.toAbsolutePath());
+
+        Path pathToReal = Paths.get("bar.txt");
+        try {
+            // This method throws an exception if the file does not exist or cannot be accessed. 
+            log.info("path: {}, path to real", pathToReal, pathToReal.toRealPath());
+        } catch (NoSuchFileException e) {
+            log.error("file not exist: {}", pathToReal, e);
+        } catch(IOException e) {
             log.error("IOException", e);
         }
     }
@@ -143,4 +343,6 @@ public class SmallFilesDemoTest {
         Path file = getSmallFileSample();
         return file.getParent();
     }
+
+    
 }
