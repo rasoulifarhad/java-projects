d2bbaddd3fc1becb2ccfeea0ffd06033f126cc9e
Farhad rasouli <rasouli.farhad@gmail.com>
2023-07-01 07:29:15 +0330

    updated

--- java-io-project/src/main/java/com/farhad/example/io/IOStreamsDemo.java
+++ java-io-project/src/main/java/com/farhad/example/io/IOStreamsDemo.java
@@ -1,5 +1,117 @@
 package com.farhad.example.io;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
 public class IOStreamsDemo {
     
+    // Both copyWithByteStream and copyWithCharacterStream use an int variable to read to and write from. However, in CopyCharacters, the int 
+    // variable holds a character value in its last 16 bits; in CopyBytes, the int variable holds a byte value in its last 8 bits. 
+    //
+    // Character streams are often "wrappers" for byte streams. The character stream uses the byte stream to perform the physical I/O, while 
+    // the character stream handles translation between characters and bytes. FileReader, for example, uses FileInputStream, while FileWriter 
+    // uses FileOutputStream.   
+    //
+    // There are two general-purpose byte-to-character "bridge" streams: InputStreamReader and OutputStreamWriter. Use them to create character 
+    // streams when there are no prepackaged character stream classes that meet your needs.
+    public static void copyWithByteStream(String src, String dest) throws IOException {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = new FileInputStream(src);
+            out = new FileOutputStream(dest);
+
+            int c;
+            while( (c = in.read()) != -1 ) {
+                out.write(c);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    // Both copyWithByteStream and copyWithCharacterStream use an int variable to read to and write from. However, in CopyCharacters, the int 
+    // variable holds a character value in its last 16 bits; in CopyBytes, the int variable holds a byte value in its last 8 bits.
+    // 
+    // Character streams are often "wrappers" for byte streams. The character stream uses the byte stream to perform the physical I/O, while 
+    // the character stream handles translation between characters and bytes. FileReader, for example, uses FileInputStream, while FileWriter 
+    // uses FileOutputStream.
+    // 
+    // There are two general-purpose byte-to-character "bridge" streams: InputStreamReader and OutputStreamWriter. Use them to create character 
+    // streams when there are no prepackaged character stream classes that meet your needs.
+    public static void copyWithCharacterStream(String src, String dest) throws IOException {
+        FileReader in = null;
+        FileWriter out = null;
+        try {
+            in = new FileReader(src);
+            out = new FileWriter(dest);
+
+            int c;
+            while( (c = in.read()) != -1 ) {
+                out.write(c);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    // Character I/O usually occurs in bigger units than single characters. One common unit is the line: a string of characters with a line 
+    // terminator at the end. A line terminator can be a carriage-return/line-feed sequence ("\r\n"), a single carriage-return ("\r"), or a 
+    // single line-feed ("\n"). Supporting all possible line terminators allows programs to read text files created on any of the widely used 
+    // operating systems.
+    //
+    // Buffered input streams read data from a memory area known as a buffer; the native input API is called only when the buffer is 
+    // empty. Similarly, buffered output streams write data to a buffer, and the native output API is called only when the buffer is 
+    // full.
+    //
+    // There are four buffered stream classes used to wrap unbuffered streams: BufferedInputStream and BufferedOutputStream create buffered 
+    // byte streams, while BufferedReader and BufferedWriter create buffered character streams.
+    //
+    // It often makes sense to write out a buffer at critical points, without waiting for it to fill. This is known as flushing the buffer.
+    // 
+    // Some buffered output classes support autoflush, specified by an optional constructor argument. When autoflush is enabled, certain key 
+    // events cause the buffer to be flushed. For example, an autoflush PrintWriter object flushes the buffer on every invocation of println 
+    // or format.
+    //
+    // To flush a stream manually, invoke its flush method. The flush method is valid on any output stream, but has no effect unless the stream 
+    // is buffered.
+    public static void copyLines(String src, String dest) throws IOException {
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            in = new BufferedReader(new FileReader(src));
+            out = new PrintWriter(new FileWriter(dest));
+            String line;
+            while ( (line = in.readLine()) != null ) {
+                out.println(line);
+            }
+
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out !=null){
+                out.close();
+            }
+
+        }
+    }
+
 }
--- java-io-project/src/test/java/com/farhad/example/io/IOStreamsDemoTest.java
+++ java-io-project/src/test/java/com/farhad/example/io/IOStreamsDemoTest.java
@@ -1,10 +1,10 @@
 package com.farhad.example.io;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,53 +15,190 @@ public class IOStreamsDemoTest {
 
     @Test
     public void demonstrateUseByteStreamsToCopyFileOneByteAtATime() throws IOException {
-        FileInputStream in = null;
-        FileOutputStream out = null;
         String srcfileName = "src/main/resources/sourceFile.txt";
-        String destFileName = "src/main/resources/destFile.txt";
-        try {
-            in = new FileInputStream(srcfileName);
-            out = new FileOutputStream(destFileName);
+        String destFileName = "src/main/resources/destByteOutputFile.txt";
+        IOStreamsDemo.copyWithByteStream(srcfileName, destFileName);
+    }
 
-            int c;
-            while ( (c = in.read()) != -1 ) {
-                out.write(c);        
+    @Test
+    public void demonstrateCharacterStreamsToCopyFileOneCharacterAtATime() throws IOException {
+        String srcfileName = "src/main/resources/sourceFile.txt";
+        String destFileName = "src/main/resources/destCharacterOutputFile.txt";
+        IOStreamsDemo.copyWithCharacterStream(srcfileName, destFileName);
+    }
+
+    @Test
+    public void demonstrateCharacterStreamsToCopyFileOneLineAtATime() throws IOException {
+        String srcfileName = "src/main/resources/sourceFile.txt";
+        String destFileName = "src/main/resources/destLineOutputFile.txt";
+        IOStreamsDemo.copyLines(srcfileName, destFileName);
+    }
+
+    // By default, a scanner uses white space to separate tokens. (White space characters include blanks, tabs, and line terminators. For the 
+    // full list, refer to the documentation for Character.isWhitespace.(https://docs.oracle.com/javase/8/docs/api/java/lang/Character.html#isWhitespace-char-))
+    //
+    // Scanner also supports tokens for all of the Java language's primitive types (except for char), as well as BigInteger and BigDecimal. Also, 
+    // numeric values can use thousands separators. Thus, in a US locale, Scanner correctly reads the string "32,767" as representing an integer 
+    // value.
+    // 
+    // We have to mention the locale, because thousands separators and decimal symbols are locale specific.
+    @Test
+    public void demonstrateScannerReadIndividualWordsFromFileAndPrintThemOut() throws IOException {
+        String fileName = "src/main/resources/sourceFile.txt";
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader(fileName)));
+            while ( scanner.hasNext() ) {
+                log.info("{}", scanner.next());
             }
         } finally {
-            if (in != null) {
-                in.close();
-            } 
-            if (out != null) {
-                out.close();
+            if (scanner != null) {
+                scanner.close();
             }
         }
     }
 
     @Test
-    public void demonstrateCharacterStreamsToCopyFileOneCharacterAtATime() throws IOException {
-        FileReader in = null;
-        FileWriter out = null;
-        String srcfileName = "src/main/resources/sourceFile.txt";
-        String destFileName = "src/main/resources/destFile.txt";
-
+    public void demonstrateScannerReadIndividualLinesFromFileAndPrintThemOut() throws IOException {
+        String fileName = "src/main/resources/sourceFile.txt";
+        Scanner scanner = null;
         try {
-            in = new FileReader(srcfileName);
-            out = new FileWriter(destFileName);
-
-            int c;
-            while ( (c = in.read()) != -1 ) {
-                out.write(c);
+            scanner = new Scanner(new BufferedReader(new FileReader(fileName)));
+            while( scanner.hasNextLine() ) {
+                log.info("{}", scanner.nextLine());
             }
         } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (out != null) {
-                out.close();
+            if (scanner != null) {
+                scanner.close();
             }
         }
-
     }
+
+    @Test
+    public void demonstrateScannerReadIndividualTokensFromFileAndPrintThemOutUsingDelimiter() throws IOException {
+        String fileName = "src/main/resources/sourceFile.txt";
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader(fileName)));
+            scanner.useDelimiter(", ");
+            while(scanner.hasNext()) {
+                log.info("{}", scanner.next());
+            }
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }   
+    
+    // The output string is "xxxxxx.xxxxxx". The period will be a different character in some locales, because System.out is a PrintStream 
+    // object, and that class doesn't provide a way to override the default locale. We could override the locale for the whole program — or 
+    // we could just use formatting(https://docs.oracle.com/javase/tutorial/essential/io/formatting.html).
+    @Test
+    public void demonstrateScannerThatReadsListOfDoubleValueAndSumThemUp() throws IOException {
+        String fileName = "src/main/resources/usnumbers.txt";
+        Scanner scanner = null;
+        double sum = 0;
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader(fileName)));
+            scanner.useLocale(Locale.US);
+            while ( scanner.hasNext() ) {
+                if (scanner.hasNextDouble()) {
+                    double d = scanner.nextDouble();
+                    log.info("{} is double", d);
+                    sum += d;
+                } else {
+                    String s = scanner.next();
+                    log.info("{} is not double", s);
+                }
+            }
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+        log.info("Sum: {}", sum);
+    }
+
+    // Stream objects that implement formatting are instances of either PrintWriter, a character stream class, or PrintStream, a byte stream class.
+    //
+    // Like all byte and character stream objects, instances of PrintStream and PrintWriter implement a standard set of write methods for simple 
+    // byte and character output. In addition, both PrintStream and PrintWriter implement the same set of methods for converting internal data 
+    // into formatted output. Two levels of formatting are provided:
+    // 
+    // - print and println format individual values in a standard way.
+    // - format formats almost any number of values based on a format string, with many options for precise formatting.
+    //
+    // The i and r variables are formatted twice: the first time using code in an overload of print, the second time by conversion code 
+    // automatically generated by the Java compiler, which also utilizes toString. 
+    @Test
+    public void demonstrateIOStreamFormattingWithPrintAndPrintlnMethodsOfPrintStream() {
+        int i = 2; 
+        double r = Math.sqrt(i);
+
+        System.out.print("The square root of ");
+        System.out.print(i);
+        System.out.print(" is ");
+        System.out.print(r);
+        System.out.println(".");
+
+        i = 5;
+        r = Math.sqrt(i);
+        System.out.println("The square root of " + i + " is " + r + ".");
+    }
+
+    // The format method formats multiple arguments based on a format string. The format string consists of static text embedded with format 
+    // specifiers; except for the format specifiers, the format string is output unchanged.
+    //
+    //  For a complete description, see format string syntax in the API specification.(https://docs.oracle.com/javase/8/docs/api/java/util/Formatter.html#syntax)
+    // 
+    // all format specifiers begin with a % and end with a 1- or 2-character conversion that specifies the kind of formatted output being 
+    // generated. The three conversions used here are:
+    // 
+    // - d formats an integer value as a decimal value.
+    // - f formats a floating point value as a decimal value.
+    // - n outputs a platform-specific line terminator.
+    // 
+    // Here are some other conversions:
+    // 
+    // - x formats an integer as a hexadecimal value.
+    // - s formats any value as a string.
+    // - tB formats an integer as a locale-specific month name.
+    // 
+    // Except for %% and %n, all format specifiers must match an argument. If they don't, an exception is thrown.
+    // In the Java programming language, the \n escape always generates the linefeed character (\\u000A). Don't use \n 
+    // unless you specifically want a linefeed character. To get the correct line separator for the local platform, 
+    // use %n.
+    //
+    // In addition to the conversion, a format specifier can contain several additional elements that further customize the formatted output.
+    @Test
+    public void demonstrateIOStreamFormattingWithFormatMethod() {
+        int i = 2;
+        double r = Math.sqrt(i);
+        System.out.format("The square root of %d is %f.%n", i, r);
+    }
+
+    // %1$+020.10f
+    // - Begin format specifier(%)
+    // - Argument index(1$)
+    // - Flags(+0)
+    // - Width(20)
+    // - Precision(.10)
+    // - Conversion(f)
+    //
+    // Working from the right, the optional elements are:
+    // 
+    // - Precision. For floating point values, this is the mathematical precision of the formatted value. For s and other general conversions, this is the maximum width of the formatted value; the value is right-truncated if necessary.
+    // - Width. The minimum width of the formatted value; the value is padded if necessary. By default the value is left-padded with blanks.
+    // - Flags specify additional formatting options. In the Format example, the + flag specifies that the number should always be formatted with a sign, and the 0 flag specifies that 0 is the padding character. Other flags include - (pad on the right) and , (format number with locale-specific thousands separators). Note that some flags cannot be used with certain other flags or with certain conversions.
+    // - The Argument Index allows you to explicitly match a designated argument. You can also specify < to match the same argument as the previous specifier. Thus the example could have said: System.out.format("%f, %<+020.10f %n", Math.PI);
+    //
+    @Test
+    public void demonstrateIOStreamFormatingWithFormatMethodAndCustomizeFormattedOoutput() {
+        System.out.format("%f, %1$+020.10f %n", Math.PI);
+        System.out.format("%f, %<+20.10f %n", Math.PI);
+        System.out.format("%f, %<+020.10f %n", Math.PI);
+   }
 }
 
 
