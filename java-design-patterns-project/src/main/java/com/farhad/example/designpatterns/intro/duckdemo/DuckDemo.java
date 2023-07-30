d7bd9f1d0e65c3ef88d6fb4e26d9363667f74cda
Farhad rasouli <rasouli.farhad@gmail.com>
2023-07-30 18:27:27 +0330

    add decoy duck and decoy duck can not quack and fly then override quack and fly methods

--- java-design-patterns-project/src/main/java/com/farhad/example/designpatterns/intro/duckdemo/DuckDemo.java
+++ java-design-patterns-project/src/main/java/com/farhad/example/designpatterns/intro/duckdemo/DuckDemo.java
@@ -15,7 +15,7 @@ public class DuckDemo {
 		}
 
 		public void fly() {
-			System.out.println("Displaying: duck..");
+			System.out.println("Flying: duck..");
 		}
 
 		public void display() {
@@ -53,26 +53,63 @@ public class DuckDemo {
 		public void quack() {
 			System.out.println("Error: Rubber duck can not Quacking.");
 		}
+
+		@Override
+		public void fly() {
+			System.out.println("Error: Rubber duck can not flying.");
+		}
+		
 		
 	}
 	
+	public static class DecoyDuck extends Duck {
+
+		@Override
+		public void display() {
+			System.out.println("Displaying: Decoy duck..");
+		}
+
+		@Override
+		public void quack() {
+			System.out.println("Error: decoy duck can not Quacking.");
+		}
+
+		@Override
+		public void fly() {
+			System.out.println("Error: decoy duck can not flying.");
+		}
+		
+	}
 
 	public static void main(String[] args) {
-		List<Duck> ducks = Arrays.asList(new MallardDuck(), new RedheadDuck(), new RubberDuck());
+		List<Duck> ducks = Arrays.asList(new MallardDuck(), new RedheadDuck(), new RubberDuck(), new DecoyDuck());
 		quack(ducks);
 		swim(ducks);
+		fly(ducks);
 		display(ducks);
 	}
 
+	private static void fly(List<Duck> ducks) {
+		System.out.println();
+		System.out.println("Flying.......");
+		ducks.forEach(Duck::fly);
+	}
+
 	private static void quack(List<Duck> ducks) {
+		System.out.println();
+		System.out.println("Quacking.......");
 		ducks.forEach(Duck::quack);
 	}
 
 	private static void swim(List<Duck> ducks) {
+		System.out.println();
+		System.out.println("Swiming.......");
 		ducks.forEach(Duck::swim);
 	}
 
 	private static void display(List<Duck> ducks) {
+		System.out.println();
+		System.out.println("Displaying.......");
 		ducks.forEach(Duck::display);
 	}
 
