f3dfa400948069882d95c9dcfa51955717f12160
Farhad Rasouli <f.rasouli@mydigipay.com>
2023-08-14 17:47:21 +0330

    updated

--- java-design-patterns-project/src/main/java/com/farhad/example/designpatterns/builder/functional/DeviceBuilderDEmo02.java
+++ java-design-patterns-project/src/main/java/com/farhad/example/designpatterns/builder/functional/DeviceBuilderDEmo02.java
@@ -3,6 +3,7 @@ package com.farhad.example.designpatterns.builder.functional;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.Value;
@@ -40,25 +41,37 @@ public class DeviceBuilderDEmo02 {
         @SuperBuilder
         @Getter
         @ToString
-        static class Device {
+        abstract static class Device {
 
             private final String name;
             private final UUID hardwareUuid;
             @Builder.Default
-            private UUID deviceUuid = UUID.randomUUID();
+            private final UUID deviceUuid = UUID.randomUUID();
             @Builder.Default
-            private ZonedDateTime created = ZonedDateTime.now();
+            private final ZonedDateTime created = ZonedDateTime.now();
+        }
+
+        @Value
+        @SuperBuilder
+        @EqualsAndHashCode(callSuper = true)
+        static class Tracker extends Device {
+
+            double lat;
+            double lon;
+            int battery;
         }
 
         public static void main(String[] args) {
-            Device device = 
-                Device.builder()
-                    .name("a device")
-                    .hardwareUuid(UUID.randomUUID())
-                    .deviceUuid(UUID.randomUUID())
-                    .created(ZonedDateTime.now())
-                    .build();
-            System.out.println(device);
+
+            Tracker tracker = 
+                Tracker.builder()
+                            .name("a tracker")
+                            .hardwareUuid(UUID.randomUUID())
+                            .lat(13.00000)
+                            .lon(52.00000)
+                            .battery(80)
+                            .build();
+            System.out.println(tracker);
         }
     }
 
