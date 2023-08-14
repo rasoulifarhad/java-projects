12134f91f499414edcf58b7c5c01ca02a8a09c59
Farhad Rasouli <f.rasouli@mydigipay.com>
2023-08-14 17:28:29 +0330

    updated

--- java-design-patterns-project/src/main/java/com/farhad/example/designpatterns/builder/functional/DeviceBuilderDEmo02.java
+++ java-design-patterns-project/src/main/java/com/farhad/example/designpatterns/builder/functional/DeviceBuilderDEmo02.java
@@ -3,12 +3,14 @@ package com.farhad.example.designpatterns.builder.functional;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 import lombok.Value;
+import lombok.experimental.SuperBuilder;
 
 public class DeviceBuilderDEmo02 {
     
-    public static class Demo02 {
+    public static class Demo01 {
 
         @Builder
         @Value
@@ -29,6 +31,34 @@ public class DeviceBuilderDEmo02 {
                                 .hardwareUuid(UUID.randomUUID())
                                 .build();
             System.out.println(device); 
+        }
+    }
+
+    static class Demo02 {
+
+
+        @SuperBuilder
+        @Getter
+        @ToString
+        static class Device {
+
+            private final String name;
+            private final UUID hardwareUuid;
+            @Builder.Default
+            private UUID deviceUuid = UUID.randomUUID();
+            @Builder.Default
+            private ZonedDateTime created = ZonedDateTime.now();
+        }
+
+        public static void main(String[] args) {
+            Device device = 
+                Device.builder()
+                    .name("a device")
+                    .hardwareUuid(UUID.randomUUID())
+                    .deviceUuid(UUID.randomUUID())
+                    .created(ZonedDateTime.now())
+                    .build();
+            System.out.println(device);
         }
     }
 
