f10d32b6c93ab539fda9c789d8760f74e77f5e03
Farhad Rasouli <f.rasouli@mydigipay.com>
2023-08-16 13:37:49 +0330

    updated

---
+++ code-kata-project/src/main/java/com/farhad/example/codekata/productarrayexceptself/ProductOfArrayExceptSelfDemo02.java
@@ -0,0 +1,62 @@
+package com.farhad.example.codekata.productarrayexceptself;
+
+import java.util.StringJoiner;
+
+public class ProductOfArrayExceptSelfDemo02 {
+    
+    static class Solution {
+
+        public int [] productArray(int [] ints) {
+
+            int [] intsResult = new int [ints.length];
+            for (int i = 0; i < intsResult.length; i++) {
+                intsResult[i] = 1;
+            }
+
+            for (int i = 0; i < ints.length; i++) {
+                for (int j = 0; j < ints.length; j++) {
+                    if ( i == j ) {
+                        continue;
+                    }
+                    intsResult[i] *= ints[j];
+                }
+            }
+            return intsResult;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int [] ints = {1, 2, 3, 4};
+        int [] res = solution.productArray(ints);
+        System.out.println(toString(ints) + " -> " + toString(res));
+
+        ints = new int[] {4, 2, 1, 5, 3};
+        res = solution.productArray(ints);
+        System.out.println(toString(ints) + " -> " + toString(res));
+
+        ints = new int[] {1, 1, 1, 1, 1};
+        res = solution.productArray(ints);
+        System.out.println(toString(ints) + " -> " + toString(res));
+
+        ints = new int[] {0, 0, 0, 0, 0};
+        res = solution.productArray(ints);
+        System.out.println(toString(ints) + " -> " + toString(res));
+
+        ints = new int[] {2, 3, 0, 4};
+        res = solution.productArray(ints);
+        System.out.println(toString(ints) + " -> " + toString(res));
+    }
+
+
+    private static String toString(int [] ints) {
+        StringJoiner joiner = new StringJoiner(",", "[", "]");
+        for (int i : ints) {
+            joiner.add(Integer.toString(i));
+        }
+        return joiner.toString();
+    }
+
+}
