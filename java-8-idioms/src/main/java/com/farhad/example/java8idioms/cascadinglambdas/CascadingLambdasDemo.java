9e3354f5e0c35ebfd576226041e2934cbbea3c39
Farhad rasouli <rasouli.farhad@gmail.com>
2023-08-26 02:59:55 +0330

    updated

--- java-8-idioms/src/main/java/com/farhad/example/java8idioms/cascadinglambdas/CascadingLambdasDemo.java
+++ java-8-idioms/src/main/java/com/farhad/example/java8idioms/cascadinglambdas/CascadingLambdasDemo.java
@@ -4,6 +4,7 @@ import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class CascadingLambdasDemo {
@@ -38,6 +39,55 @@ public class CascadingLambdasDemo {
 				.collect(toList())
 		);
 
+		List<Integer> valuesOver25 = numbers1.stream()
+			.filter(e -> e > 25 )
+			.collect(toList());
+		System.out.println(valuesOver25);
+
+		List<Integer> valuesOver50 = numbers1.stream()
+			.filter(e -> e > 50 )
+			.collect(toList());
+		System.out.println(valuesOver50);
+		
+		List<Integer> valuesOver75 = numbers1.stream()
+			.filter(e -> e > 75 )
+			.collect(toList());
+		System.out.println(valuesOver75	);
+
+		// While the two lambdas in the previous example were identical, there are slight variations in the 
+		// three above. Creating a Function that returns a Predicate solves the problem at hand.
+		// 
+		// The reference isGreaterThan refers to a lambda expression that represents a Function<T, U>—or more 
+		// precisely, Function<Integer, Predicate<Integer>>. The input is an Integer and the output is a 
+		// Predicate<Integer>.
+		//
+		// Within the body of the lambda expression (the outer {}) we created another reference, isGreaterThanPivot, 
+		// that holds a reference to another lambda expression. This time the reference is to a Predicate instead of 
+		// a Function. Finally, we return that reference.
+		// 
+		// isGreaterThan is a reference to a lambda expression that returns another lambda expression when 
+		// invoked—in other words, there’s a cascade of lambdas hidden in there.
+		Function<Integer, Predicate<Integer>> isGreaterThan = (Integer pivot) -> {
+			Predicate<Integer> isGreaterThanPivot = (Integer candidate) -> {
+				return candidate > pivot;
+			};
+			return isGreaterThanPivot;
+		};
+
+		valuesOver25 = numbers1.stream()
+			.filter(isGreaterThan.apply(25))
+			.collect(toList());
+		System.out.println(valuesOver25);
+
+		valuesOver50 = numbers1.stream()
+			.filter(isGreaterThan.apply(50))
+			.collect(toList());
+		System.out.println(valuesOver50);
+
+		valuesOver75 = numbers1.stream()
+			.filter(isGreaterThan.apply(75))
+			.collect(toList());
+		System.out.println(valuesOver75);
 	}
  
 	public static int totalSelectedValues(List<Integer> values, Predicate<Integer> selector) {
