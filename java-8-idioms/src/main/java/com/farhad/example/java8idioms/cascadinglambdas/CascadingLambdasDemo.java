d4bc68b6582fdfe339d6a1f6f1b3685448b37737
Farhad rasouli <rasouli.farhad@gmail.com>
2023-08-25 23:07:48 +0330

    updated

--- java-8-idioms/src/main/java/com/farhad/example/java8idioms/cascadinglambdas/CascadingLambdasDemo.java
+++ java-8-idioms/src/main/java/com/farhad/example/java8idioms/cascadinglambdas/CascadingLambdasDemo.java
@@ -1,5 +1,7 @@
 package com.farhad.example.java8idioms.cascadinglambdas;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
@@ -15,6 +17,27 @@ public class CascadingLambdasDemo {
 
 		Predicate<Integer> isOdd = createIsOdd();
 		System.out.println(isOdd.test(3));
+
+		List<Integer> numbers1 = Arrays.asList(10, 30, 50, 70);
+		System.out.println(extractValuesGreaterThan50(numbers1));
+		List<Integer> numbers2 = Arrays.asList(50, 70, 90, 110);
+		System.out.println(extractValuesGreaterThan50AndDouble(numbers2));
+
+		Predicate<Integer> isGreaterThan50 = number -> number > 50;
+
+		System.out.println(
+			numbers1.stream()
+				.filter(isGreaterThan50)
+				.collect(toList())
+		);
+
+		System.out.println(
+			numbers2.stream()
+				.filter(isGreaterThan50)
+				.map(e -> e * 2)
+				.collect(toList())
+		);
+
 	}
  
 	public static int totalSelectedValues(List<Integer> values, Predicate<Integer> selector) {
@@ -30,4 +53,20 @@ public class CascadingLambdasDemo {
 	public static Predicate<Integer> createIsOdd() {
 		return  number -> number %  2 != 0;
 	}
+
+	public static List<Integer> extractValuesGreaterThan50(List<Integer> numbers) {
+		return numbers.stream()
+					.filter(e ->  e > 50)
+					.collect(toList());
+	}
+
+	public static List<Integer> extractValuesGreaterThan50AndDouble(List<Integer> numbers) {
+		return numbers.stream()
+					.filter(e ->  e > 50)
+					.map(e -> e * 2)
+					.collect(toList());
+	}
+
+	
+
 }
