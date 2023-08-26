84f524c0b513f8bc690ea5360e3f47859bb44609
Farhad rasouli <rasouli.farhad@gmail.com>
2023-08-27 03:16:19 +0330

    updated

---
+++ java-8-idioms/src/main/java/com/farhad/example/java8idioms/functionalpipelinesandstream/FunctionalPipelinesAndStreamDemo.java
@@ -0,0 +1,7 @@
+package com.farhad.example.java8idioms.functionalpipelinesandstream;
+
+// See https://developer.ibm.com/articles/j-java8idioms11/
+public class FunctionalPipelinesAndStreamDemo {
+	
+
+}
