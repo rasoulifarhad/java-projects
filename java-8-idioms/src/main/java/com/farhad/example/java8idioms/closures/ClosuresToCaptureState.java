2767d4f41242b24c8250df4088a64735aae5bad6
Farhad rasouli <rasouli.farhad@gmail.com>
2023-08-26 20:41:03 +0330

    updated

--- java-8-idioms/src/main/java/com/farhad/example/java8idioms/closures/ClosuresToCaptureState.java
+++ java-8-idioms/src/main/java/com/farhad/example/java8idioms/closures/ClosuresToCaptureState.java
@@ -13,6 +13,7 @@ public class ClosuresToCaptureState {
 		System.out.println(doubleEvenElements(Arrays.asList(1, 2, 3, 4)));
 
 		print();
+		lambdaPrint();
 	}
 
 	private static List<Integer> doubleEvenElements(List<Integer> numbers) {
@@ -28,7 +29,18 @@ public class ClosuresToCaptureState {
 	// expect variables referenced to be in scope, as well. Some variables are received as parameters to 
 	// functions or lambdas. Some variables are defined locally. And some variables come from outside the 
 	// function, in what's called the lexical scope.
+	// 
+	// A lexical scope is the defining scope of a function. In turn, it may also be the defining scope of 
+	// the defining scope, and so on.
 	//
+	// In the previous code, the method run does not define location or receive it as a parameter. The defining 
+	// scope of run is the anonymous inner object of Runnable. Because location is not defined as a field in that 
+	// instance, the search continues to the defining scope of the anonymous inner object—in this case the local 
+	// scope of the method print.
+
+    // If location were not present in the scope, the compiler's search would continue in the defining scope of 
+	// print, continuing until either the variable was found or the search failed.
+	// 
 	public static void print() {
 		String location = "World";
 
@@ -40,6 +52,15 @@ public class ClosuresToCaptureState {
 			}
 			
 		};
+
+		runnable.run();
+	}
+
+	public static void lambdaPrint() {
+
+		String location = "World";
+
+		Runnable runnable = () -> System.out.println("Hello " + location);
 
 		runnable.run();
 	}
