ea4bd4c7784ff16084bbe4e873b82dae3dd61a35
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-02 07:57:13 +0330

    updated

---
+++ design-principles-project/src/main/java/com/farhad/example/designpatterns/anotheropenclosedprinciple/first/ResumeTemplateProcess.java
@@ -0,0 +1,25 @@
+package com.farhad.example.designpatterns.anotheropenclosedprinciple.first;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ResumeTemplateProcess {
+	private final TemplateRender templateRender;
+
+	public String processResumeTemplate(Map<String, Object> data) {
+		if (!isValid(data)) {
+			throw new IllegalArgumentException("Not valid data");
+		}
+		data.put("validUntil", LocalDateTime.now());
+		return templateRender.render("resumeTemplate.template", data);
+	}
+
+	private boolean isValid(Map<String, Object> data) {
+		return data != null && !data.isEmpty() && data.containsKey("personName");
+	}
+
+	
+}
--- design-principles-project/src/main/java/com/farhad/example/designpatterns/anotheropenclosedprinciple/note.md
+++ design-principles-project/src/main/java/com/farhad/example/designpatterns/anotheropenclosedprinciple/note.md
@@ -1,1 +1,3 @@
 See https://coderstower.com/2020/01/13/open-close-principle-by-example/
+
+> As a business, I want to generate a person’s resume in PDF with the person’s name and the date until the resume is valid which is the current date
