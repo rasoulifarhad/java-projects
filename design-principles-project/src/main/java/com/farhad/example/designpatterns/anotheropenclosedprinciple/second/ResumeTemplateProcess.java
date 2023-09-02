6c8a0211ebd731fec55aad77fb5c83791e193607
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-02 08:30:39 +0330

    updated

---
+++ design-principles-project/src/main/java/com/farhad/example/designpatterns/anotheropenclosedprinciple/second/ResumeTemplateProcess.java
@@ -0,0 +1,45 @@
+package com.farhad.example.designpatterns.anotheropenclosedprinciple.second;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import com.farhad.example.designpatterns.anotheropenclosedprinciple.first.TemplateRender;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ResumeTemplateProcess {
+	private final TemplateRender templateRender;
+
+	public String processBasicResumeTemplate(Map<String, Object> data) {
+		if (!isValidBasicResume(data)) {
+			throw new IllegalArgumentException("Not valid data for basic");
+		}
+		data.put("validUntil", LocalDateTime.now());
+		return templateRender.render("basicResumeTemplate.template", data);
+	}
+
+	public String processLongResumeTemplate(Map<String, Object> data) {
+		if (!isValidLongResumeTemplate(data)) {
+			throw new IllegalArgumentException("Not valid data for long resume");
+		}
+
+		data.put("validUntil", LocalDateTime.now().plusDays(7));
+		return templateRender.render("longResumeTemplate.template", data);
+	}
+
+	private boolean isValidLongResumeTemplate(Map<String, Object> data) {
+		return data != null && 
+			   !data.isEmpty() &&
+			   data.containsKey("personName") &&
+			   data.containsKey("description");	
+	}
+
+	private boolean isValidBasicResume(Map<String, Object> data) {
+		return data != null && 
+			   !data.isEmpty() && 
+			   data.containsKey("personName");
+	}
+
+	
+}
