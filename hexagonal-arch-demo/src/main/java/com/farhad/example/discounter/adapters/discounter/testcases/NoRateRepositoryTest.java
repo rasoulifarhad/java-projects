dd51c86a06b8961d92ff197cd55a8aaf55dbaa92
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-07 19:30:39 +0330

    updated

---
+++ hexagonal-arch-demo/src/main/java/com/farhad/example/discounter/adapters/discounter/testcases/NoRateRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.farhad.example.discounter.adapters.discounter.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.farhad.example.discounter.application.Amount;
+import com.farhad.example.discounter.application.DiscounterApp;
+
+public class NoRateRepositoryTest {
+	
+
+	@Test
+	public void discount_of_0_should_be_0() {
+		// given
+		DiscounterApp discounterApp = new DiscounterApp(null);
+		//when
+		Amount currentDiscount = discounterApp.calculateDiscount(Amount.parse("0.00"));
+		Amount expectedDiscount = Amount.parse("0.00");
+		//then
+		Assert.assertTrue(currentDiscount.isEqualTo(expectedDiscount));
+	}
+
+	@Test
+	public void discount_of_100_should_be_8() {
+		//given
+		DiscounterApp discounterApp = new DiscounterApp(null);
+		//when
+		Amount currentAmount = discounterApp.calculateDiscount(Amount.parse("100.00"));
+		Amount expectedDiscount = Amount.parse("8.00");
+		Assert.assertTrue(currentAmount.isEqualTo(expectedDiscount));
+	}
+
+	@Test
+	public void discount_of_50_should_be_4() {
+		//given
+		DiscounterApp discounterApp = new DiscounterApp(null);
+		//when
+		Amount currentAmount = discounterApp.calculateDiscount(Amount.parse("50.00"));
+		Amount expectedDiscount = Amount.parse("4.00");
+		Assert.assertTrue(currentAmount.isEqualTo(expectedDiscount));
+	}
+
+	@Test
+	public void discount_of_200_should_be_16() {
+		//given
+		DiscounterApp discounterApp = new DiscounterApp(null);
+		//when
+		Amount currentAmount = discounterApp.calculateDiscount(Amount.parse("200.00"));
+		Amount expectedDiscount = Amount.parse("16.00");
+		Assert.assertTrue(currentAmount.isEqualTo(expectedDiscount));
+	}
+}
---
+++ hexagonal-arch-demo/src/main/java/com/farhad/example/discounter/adapters/discounter/testcases/TestRunner.java
@@ -0,0 +1,57 @@
+package com.farhad.example.discounter.adapters.discounter.testcases;
+
+import java.io.File;
+import java.time.Instant;
+
+import org.testng.TestNG;
+
+import com.farhad.example.discounter.adapters.Driver;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TestRunner implements Driver {
+
+	private static final String OUTPUT_DIRECTORY = 
+		System.getProperty("user.home") + File.separator +
+		"discounterApp" + File.separator +
+		"test-output" + File.separator + 
+		Instant.now().toEpochMilli();
+
+	private final String rateRepositoryType;
+
+
+	@Override
+	public void run(String... args) {
+		System.out.println("==========================================================");
+		System.out.println("Running test cases driver using testNG framework");
+		System.out.println("Html report will be generated to the following directory:");
+		System.out.println(OUTPUT_DIRECTORY);
+		System.out.println("===========================================================");
+		TestNG testNG = new TestNG();
+		testNG.setOutputDirectory(OUTPUT_DIRECTORY);
+		testNG.setTestClasses(testClassesToRun());
+		testNG.run();
+	}
+
+
+	private Class[] testClassesToRun() {
+		if (rateRepositoryType == null) {
+			return new Class[] {NoRateRepositoryTest.class};
+		} 
+		if (rateRepositoryType.trim().isEmpty()) {
+			return new Class[] {NoRateRepositoryTest.class};
+		}
+		if (rateRepositoryType.trim().equals("none")) {
+			return new Class[] {NoRateRepositoryTest.class};
+		}
+		if (rateRepositoryType.trim().equals("stub")) {
+			return new Class[] {};
+		}
+		if (rateRepositoryType.trim().equals("file")) {
+			return new Class[] {};
+		}
+		return new Class[] {};
+	}
+	
+}
