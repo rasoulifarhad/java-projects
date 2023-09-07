4d8932741e43967462705aad20b7d4bfc727427c
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-07 17:06:19 +0330

    updated

--- hexagonal-arch-demo/src/main/java/com/farhad/example/discounter/adapters/discounter/cli/CommandLineInterface.java
+++ hexagonal-arch-demo/src/main/java/com/farhad/example/discounter/adapters/discounter/cli/CommandLineInterface.java
@@ -1,6 +1,9 @@
 package com.farhad.example.discounter.adapters.discounter.cli;
 
+import java.util.Scanner;
+
 import com.farhad.example.discounter.adapters.Driver;
+import com.farhad.example.discounter.application.Amount;
 import com.farhad.example.discounter.application.ports.Discounter;
 
 import lombok.RequiredArgsConstructor;
@@ -12,8 +15,59 @@ public class CommandLineInterface implements Driver {
 	
 	@Override
 	public void run(String... args) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'run'");
+		System.out.println("============================");
+		System.out.println("Running CLI");
+		System.out.println("============================");
+		Scanner scanner = new Scanner(System.in);
+		boolean exit = false;
+		while (!exit) {
+			printAppMenu();	
+			try {
+				String option = scanner.next();
+				switch (option) {
+					case "1":
+						option1(scanner);
+						break;
+					case "2":
+						exit = true;
+						break;
+				
+					default:
+						System.out.println("Invalid option");
+						break;
+				}
+			} catch (Exception e) {
+				System.out.println("Unexpected error");
+				e.printStackTrace();
+			}
+		}
 	}
-	 
+
+	private void option1(Scanner scanner) {
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Enter an amount:");
+		String amount = scanner.next();
+		System.out.println("The discount is : ");
+		Amount discount = discounterApp.calculateDiscount(Amount.parse(amount));
+		System.out.println(discount.toString());
+
+	}
+
+	private void printAppMenu() {
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("-------------------");
+		System.out.println("DISCOUNTER APP MENU");
+		System.out.println("-------------------");
+		System.out.println("(1) Calculate discount.");
+		System.out.println("(2) Exit.");
+		System.out.println("Choose an option:");	
+	}
+ 
 }
