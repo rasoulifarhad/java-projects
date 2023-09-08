d9c9aec6d219e508d25447ee05c46870757b9b97
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-08 18:59:46 +0330

    updated

--- java-design-patterns-02/src/main/java/com/farhad/example/nestedbuilder/carbuilder/Car.java
+++ java-design-patterns-02/src/main/java/com/farhad/example/nestedbuilder/carbuilder/Car.java
@@ -7,5 +7,49 @@ import lombok.Data;
 @Data
 public class Car {
 	private Engine engine;
-	private List<Wheel> Wheels;
+	private List<Wheel> wheels;
+
+	private Car() {
+
+	}
+
+	private Car(Builder builder) {
+		setEngine(builder.engine);
+		setWheels(builder.wheels);
+	}
+
+	public static Builder newBuilder() {
+		return new Builder();
+	}
+
+	public static Builder newBuilder(Car copy) {
+		Builder builder = new Builder();
+		builder.engine = copy.engine;
+		builder.wheels = copy.wheels;
+		return builder;
+	}
+
+	public static final class Builder {
+
+		private Engine engine;
+		private List<Wheel> wheels;
+
+		private Builder() {
+
+		}
+
+		public Builder withEngine(Engine engine) {
+			this.engine = engine;
+			return this;
+		}
+
+		public Builder withWheels(List<Wheel> wheels) {
+			this.wheels = wheels;
+			return this;
+		}
+
+		public Car builde() {
+			return new Car(this);
+		}
+	}
 }
--- java-design-patterns-02/src/main/java/com/farhad/example/nestedbuilder/carbuilder/Engine.java
+++ java-design-patterns-02/src/main/java/com/farhad/example/nestedbuilder/carbuilder/Engine.java
@@ -6,4 +6,48 @@ import lombok.Data;
 public class Engine {
 	private int power;
 	private int type;
+
+	private Engine() {
+
+	}
+
+	private Engine(Builder builder) {
+		setPower(builder.power);
+		setType(builder.type);
+	}
+
+	public static Builder newBuilder() {
+		return new Builder();
+	}
+
+	public static Builder newBuilder(Engine copy) {
+		Builder builder = new Builder();
+		builder.power = copy.power;
+		builder.type = copy.type;
+		return builder;
+	}
+
+	public static final class Builder {
+
+		private int power;
+		private int type;
+
+		private Builder() {
+
+		}
+
+		public Builder withPower(int power) {
+			this.power = power;
+			return this;
+		}
+
+		public Builder withType(int type) {
+			this.type = type;
+			return this;
+		}
+
+		public Engine build() {
+			return new Engine(this);
+		}
+	}
 }
