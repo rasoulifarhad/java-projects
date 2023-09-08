d56c0f328414a76fc2b3e4de66593ab099f67958
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-08 19:03:54 +0330

    updated

---
+++ java-design-patterns-02/src/main/java/com/farhad/example/nestedbuilder/carbuilder/CarBuilderClient.java
@@ -0,0 +1,23 @@
+package com.farhad.example.nestedbuilder.carbuilder;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarBuilderClient {
+	
+	public static void main(String[] args) {
+		Engine engine = Engine.newBuilder().withPower(100).withType(5).build();
+		Wheel wheel1 = Wheel.newBuilder().withSize(4).withColor(3).withType(2).build();
+		Wheel wheel2 = Wheel.newBuilder().withSize(4).withColor(3).withType(2).build();
+		Wheel wheel3 = Wheel.newBuilder().withSize(4).withColor(3).withType(2).build();
+
+		List<Wheel> wheels = Arrays.asList(
+			wheel1,
+			wheel2,
+			wheel3);
+
+		Car car = Car.newBuilder().withEngine(engine).withWheels(wheels).builde();
+
+		System.out.println("Car: " + car);
+	}
+}
