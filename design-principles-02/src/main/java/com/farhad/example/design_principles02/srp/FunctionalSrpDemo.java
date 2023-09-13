fef860bd11b30c2c28fdb7876008e2e9297ab140
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-14 03:07:32 +0330

    updated

--- design-principles-02/src/main/java/com/farhad/example/design_principles02/srp/FunctionalSrpDemo.java
+++ design-principles-02/src/main/java/com/farhad/example/design_principles02/srp/FunctionalSrpDemo.java
@@ -1,6 +1,10 @@
 package com.farhad.example.design_principles02.srp;
 
+import static com.farhad.example.design_principles02.srp.FunctionalSrpDemo.FunctionalCombinatorSrp.ArtistValidator.hasEmailWithAtSign;
+import static com.farhad.example.design_principles02.srp.FunctionalSrpDemo.FunctionalCombinatorSrp.ArtistValidator.nameIsNotEmpty;
+
 import java.util.Optional;
+import java.util.function.Function;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -48,10 +52,30 @@ public class FunctionalSrpDemo {
 			private final String email;
 		}
 
+		interface ArtistValidator extends Function<Artist, Optional<Artist>> {
+			static ArtistValidator hasEmailWithAtSign() {
+				return holds(artist -> artist.email.contains("@"));
+			}
+
+			static ArtistValidator nameIsNotEmpty() {
+				return holds(artist -> !artist.name.trim().isEmpty()); 
+			}
+
+			static ArtistValidator holds(Function<Artist, Boolean> func) {
+				return artist ->  func.apply(artist) ? Optional.of(artist) : Optional.empty();
+			} 
+
+			default ArtistValidator and(ArtistValidator other) {
+				return artist -> apply(artist).flatMap(other);
+			}
+		}
+
 		class ArtistService {
 
-			// public Optional<Artist> createArtistWithValidatedEmailAndName(String name, String emailString){
-			// }
+			public Optional<Artist> createArtistWithValidatedEmailAndName(String name, String emailString){
+				final ArtistValidator validator = nameIsNotEmpty().and(hasEmailWithAtSign());
+				return validator.apply(new Artist(name, emailString));
+			}
 		}
 	}
 }
