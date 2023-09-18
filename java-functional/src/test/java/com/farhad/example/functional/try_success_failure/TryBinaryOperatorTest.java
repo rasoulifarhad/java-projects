c1c8390d14476ed0ba2a8cf614b8486fa319b924
Farhad rasouli <rasouli.farhad@gmail.com>
2023-09-18 12:53:29 +0330

    updated

--- java-functional/src/test/java/com/farhad/example/functional/try_success_failure/TryBinaryOperatorTest.java
+++ java-functional/src/test/java/com/farhad/example/functional/try_success_failure/TryBinaryOperatorTest.java
@@ -1,5 +1,7 @@
 package com.farhad.example.functional.try_success_failure;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -19,6 +21,153 @@ public class TryBinaryOperatorTest {
 		Try<Integer> result =  Try.apply(this::failure);
 
 		assertTrue(result.isFailure(), "result must be a failure");
+	}
+
+	@Test
+	public void testGetAgainstASuccess() {
+		Try<Integer> result = Try.apply(this::success);
+
+		int intResult = result.get();
+		assertThat(intResult).isEqualTo(42);
+	}
+
+	@Test
+	public void testGetAgainstAFailure() {
+		Try<Integer> result = Try.apply(this::failure);
+
+		assertThrows(GetOfFailureException.class, () -> result.get());
+	}
+
+	@Test
+	public void testUncheckedGetAgainstASuccess() throws Exception {
+		Try<Integer> result = Try.apply(this::success);
+		int intResult = result.checkedGet();
+
+		assertThat(intResult).isEqualTo(42);
+	}
+
+	@Test
+	public void testUncheckedGetAgainstAFailure() throws Exception {
+		Try<Integer> result = Try.apply(this::failure);
+
+		assertThrows(NumberFormatException.class, () -> result.checkedGet());
+	}
+
+	@Test
+	public void testForEachAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testForEachAgainstAFailure() {
+		
+	}
+
+
+	@Test
+	public void testMapAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testMapAgainstAFailure() {
+		
+	}
+
+	@Test
+	public void testFlatMapAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testFlatMapAgainstAFailure() {
+		
+	}
+
+	@Test
+	public void testFilterAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testFilterAgainstAFailure() {
+		
+	}
+
+	@Test
+	public void testNonMatchingFilter() {
+
+	}
+
+	@Test
+	public void testRecoverAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testRecoverAgainstAFailure() {
+		
+	}
+
+		@Test
+	public void testRecoverWithAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testRecoverWithAgainstAFailure() {
+		
+	}
+
+	@Test
+	public void testFailedAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testFailedAgainstAFailure() {
+		
+	}
+
+	@Test
+	public void testToOptionalAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testToOptionalAgainstAFailure() {
+		
+	}
+
+		@Test
+	public void testGetOrElseAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testGetOrElseAgainstAFailure() {
+		
+	}
+
+
+	@Test
+	public void testOrElseAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testOrElseAgainstAFailure() {
+		
+	}
+
+		@Test
+	public void testTransformAgainstASuccess() {
+
+	}
+
+	@Test
+	public void testTransformAgainstAFailure() {
+		
 	}
 
 	private int success() {
