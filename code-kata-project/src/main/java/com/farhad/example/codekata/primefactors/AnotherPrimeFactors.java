9932d1d4e35a72496b45a2b060a7be7ef46a2ea9
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-19 21:21:14 +0330

    updated

--- code-kata-project/src/main/java/com/farhad/example/codekata/primefactors/AnotherPrimeFactors.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/primefactors/AnotherPrimeFactors.java
@@ -9,6 +9,32 @@ public class AnotherPrimeFactors {
         return factorsOf(n, 2, new ArrayList<Integer>());
     }
 
+    public static List<Integer> anotherFactorsOf(int n) {
+        return anotherFactorsOf(n, 2, new ArrayList<Integer>());
+    }
+
+    public static List<Integer> anotherFactorsOf02(int n) {
+        return anotherFactorsOf02(n, 2, new ArrayList<Integer>());
+    }
+
+    public static List<Integer> anotherFactorsOf03(int n) {
+        return anotherFactorsOf03(n, 2, new ArrayList<Integer>());
+    }
+
+    // make this more interesting by using an enum and a switch.
+    public static List<Integer> anotherFactorsOfWithEnumAndSwitch(int n) {
+        return anotherFactorsOfWithEnumAndSwitch(n, 2, new ArrayList<Integer>());
+    }
+
+    // Now let’s move the determination of the next state into each case.
+    public static List<Integer> anotherFactorsOfWithEnumAndSwitch02(int n) {
+        return anotherFactorsOfWithEnumAndSwitch02(n, 2, new ArrayList<Integer>());
+    }
+
+    public static List<Integer> anotherFactorsOfWithEnumAndSwitch03(int n) {
+        return anotherFactorsOfWithEnumAndSwitch03(n, 2, new ArrayList<Integer>());
+    }
+
     private static List<Integer> factorsOf(int n, int d, ArrayList<Integer> factors) {
         while (true) {
             if( n > 1 ) {
@@ -76,6 +102,102 @@ public class AnotherPrimeFactors {
                 return factors;
             }
         }
+    }
+
+    private enum State {Starting, Factoring, Searching, Done}
+
+    private static List<Integer> anotherFactorsOfWithEnumAndSwitch(int n, int d, ArrayList<Integer> factors) {
+        State state = State.Starting;
+        while (true) {
+            boolean factorsRemain = n > 1;
+            boolean currentDivisorIsFactor = n % d == 0;        
+            if(factorsRemain && currentDivisorIsFactor) {
+                state = State.Factoring;
+            }
+            if(factorsRemain && !currentDivisorIsFactor) {
+                state = State.Searching;
+            }
+            if(!factorsRemain) {
+                state = State.Done;
+            }
+            switch (state) {
+                case Factoring:
+                    factors.add(d);
+                    n /= d;
+                    break;
+                case Searching:
+                    d++;
+                    break;
+                case Done:
+                    return factors;
+            }            
+        }
+
+    }
+
+    private static List<Integer> anotherFactorsOfWithEnumAndSwitch02(int n, int d, ArrayList<Integer> factors) {
+        State state = State.Starting;
+        while (true) {
+            switch (state) {
+                case Starting:
+                    if (n == 1 ) {
+                        state = State.Done;
+                    } else if (n % d == 0) {
+                        state = State.Factoring;
+                    } else {
+                        state = State.Searching;
+                    }
+                    break;
+                case Factoring:
+                    factors.add(d);
+                    n /= d;
+                    if (n == 1) {
+                        state = state.Done;
+                    } else if (n % d != 0) {
+                        state = State.Searching;
+                    }
+                    break;
+                case Searching:
+                    d++;
+                    if(n == 1) {
+                        state = State.Done;
+                    } else if (n % d == 0) {
+                        state = State.Factoring;
+                    } 
+                    break;
+                case Done:
+                    return factors;
+            }            
+        }
+
+    }
+
+    private static List<Integer> anotherFactorsOfWithEnumAndSwitch03(int n, int d, ArrayList<Integer> factors) {
+        State state = State.Starting;
+        while (true) {
+            switch (state) {
+                case Starting:
+                    break;
+                case Factoring:
+                    factors.add(d);
+                    n /= d;
+                    break;
+                case Searching:
+                    d++;
+                    break;
+                case Done:
+                    return factors;
+            } 
+            
+            if(n == 1){
+                state = State.Done;
+            } else if (n % d == 0) {
+                state = State.Factoring;
+            } else {
+                state = State.Searching;
+            }
+        }
+
     }
 
 }
--- code-kata-project/src/test/java/com/farhad/example/codekata/primefactors/AnotherPrimeFactorsTest.java
+++ code-kata-project/src/test/java/com/farhad/example/codekata/primefactors/AnotherPrimeFactorsTest.java
@@ -1,5 +1,11 @@
 package com.farhad.example.codekata.primefactors;
 
+import static com.farhad.example.codekata.primefactors.AnotherPrimeFactors.anotherFactorsOf;
+import static com.farhad.example.codekata.primefactors.AnotherPrimeFactors.anotherFactorsOf02;
+import static com.farhad.example.codekata.primefactors.AnotherPrimeFactors.anotherFactorsOf03;
+import static com.farhad.example.codekata.primefactors.AnotherPrimeFactors.anotherFactorsOfWithEnumAndSwitch;
+import static com.farhad.example.codekata.primefactors.AnotherPrimeFactors.anotherFactorsOfWithEnumAndSwitch02;
+import static com.farhad.example.codekata.primefactors.AnotherPrimeFactors.anotherFactorsOfWithEnumAndSwitch03;
 import static com.farhad.example.codekata.primefactors.AnotherPrimeFactors.factorsOf;
 
 import org.junit.jupiter.api.Test;
@@ -7,9 +13,45 @@ import org.junit.jupiter.api.Test;
 public class AnotherPrimeFactorsTest {
 
     @Test
-    void testGenerate() {
+    void testFactorsOf() {
         System.out.println(factorsOf(9));
         System.out.println(factorsOf(2222));
+    }
+
+    @Test
+    void testAnotherFactorsOf() {
+        System.out.println(anotherFactorsOf(9));
+        System.out.println(anotherFactorsOf(2222));
+    }
+
+    @Test
+    void testAnotherFactorsOf02() {
+        System.out.println(anotherFactorsOf02(9));
+        System.out.println(anotherFactorsOf02(2222));
+    }
+
+    @Test
+    void testAnotherFactorsOf03() {
+        System.out.println(anotherFactorsOf03(9));
+        System.out.println(anotherFactorsOf03(2222));
+    }
+
+    @Test
+    void testAnotherFactorsOfWithEnumAndSwitch() {
+        System.out.println(anotherFactorsOfWithEnumAndSwitch(9));
+        System.out.println(anotherFactorsOfWithEnumAndSwitch(2222));
+    }
+
+    @Test
+    void testAnotherFactorsOfWithEnumAndSwitch02() {
+        System.out.println(anotherFactorsOfWithEnumAndSwitch02(9));
+        System.out.println(anotherFactorsOfWithEnumAndSwitch02(2222));
+    }
+
+    @Test
+    void testAnotherFactorsOfWithEnumAndSwitch03() {
+        System.out.println(anotherFactorsOfWithEnumAndSwitch03(9));
+        System.out.println(anotherFactorsOfWithEnumAndSwitch03(2222));
     }
 
 }
