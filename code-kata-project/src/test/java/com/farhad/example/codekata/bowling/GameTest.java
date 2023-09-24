d468ab56dc8c7b6b82f556224e75adb920d2526e
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-25 02:23:41 +0330

    updated

--- code-kata-project/src/test/java/com/farhad/example/codekata/bowling/GameTest.java
+++ code-kata-project/src/test/java/com/farhad/example/codekata/bowling/GameTest.java
@@ -80,5 +80,58 @@ public class GameTest {
         assertEquals(300, g.score());
         assertEquals(11, g.getCurrentFrame());
     }
-    
+
+    @Test
+    public void testEndOfArray() {
+        for (int i = 0; i < 9; i++) {
+            g.add(0);
+            g.add(0);
+        }
+        g.add(2);
+        g.add(8); // 10th frame spare
+        g.add(10); // Strike in last position of array.
+        assertEquals(20, g.score());
+    }
+
+    @Test
+    public void testSampleGame() {
+        g.add(1);
+        g.add(4);
+        g.add(4);
+        g.add(5);
+        g.add(6);
+        g.add(4);
+        g.add(5);
+        g.add(5);
+        g.add(10);
+        g.add(0);
+        g.add(1);
+        g.add(7);
+        g.add(3);
+        g.add(6);
+        g.add(4);
+        g.add(10);
+        g.add(2);
+        g.add(8);
+        g.add(6);
+        assertEquals(133, g.score());
+    }
+
+    @Test
+    public void testHeartBreak() {
+        for (int i = 0; i < 11; i++)
+            g.add(10);
+        g.add(9);
+        assertEquals(299, g.score());
+    }
+
+    @Test
+    public void testTenthFrameSpare() {
+        for (int i = 0; i < 9; i++)
+            g.add(10);
+        g.add(9);
+        g.add(1);
+        g.add(1);
+        assertEquals(270, g.score());
+    }
 }
