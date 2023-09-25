42387e837f894ca2741a6c5340d38fe522fca7c2
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-26 00:24:41 +0330

    updated

--- java-design-patterns-02/src/main/java/com/farhad/example/generate_primes/refactored/PrimeGenerator.java
+++ java-design-patterns-02/src/main/java/com/farhad/example/generate_primes/refactored/PrimeGenerator.java
@@ -5,60 +5,59 @@ public class PrimeGenerator {
      * @param maxValue is the generation limit.
      */
 
-    private static int s;
-    private static boolean[] f;
-    private static int[] primes;
+    private static boolean[] isCrossed;
+    private static int[] result;
 
     public static int[] generatePrimes(int maxValue) {
         if (maxValue < 2) {
             return new int[0];
         } else {
-            initializeSieve(maxValue);
+            initializeArrayOfIntegers(maxValue);
             sieve();
             loadPrimes();
-            return primes;
+            return result;
         }
     }
 
-    private static void initializeSieve(int maxValue) {
-        int s = maxValue + 1; // size of array
-        boolean[] f = new boolean[s];
-        int i;
+    private static void initializeArrayOfIntegers(int maxValue) {
+        isCrossed = new boolean[maxValue + 1];
+        isCrossed[0] = isCrossed[1] = false;
         // initialize array to true.
-        for (i = 0; i < s; i++) {
-            f[i] = true;
+        for (int i = 2; i < isCrossed.length; i++) {
+            isCrossed[i] = true;
         }
-        // get rid of known non-primes
-        f[0] = f[1] = false;
-
     }
 
     private static void sieve() {
         int i, j;
-        for (i = 2; i < Math.sqrt(s) + 1; i++) {
-            if (f[i]) {// if i is uncrossed, cross its multiples.
+        for (i = 2; i < Math.sqrt(isCrossed.length) + 1; i++) {
+            if (isCrossed(i)) {// if i is uncrossed, cross its multiples.
 
-                for (j = 2 * i; j < s; j += i) {
-                    f[j] = false; // multiple is not prime
+                for (j = 2 * i; j < isCrossed.length; j += i) {
+                    isCrossed[j] = false; // multiple is not prime
                 }
             }
         }
+    }
+
+    private static boolean isCrossed(int i) {
+        return isCrossed[i];
     }
 
     private static void loadPrimes() {
         int i, j;
         // how many primes are there?
         int count = 0;
-        for (i = 0; i < s; i++) {
-            if (f[i]) {
+        for (i = 0; i < isCrossed.length; i++) {
+            if (isCrossed[i]) {
                 count++; // bump count.
             }
         }
-        int[] primes = new int[count];
+        result = new int[count];
         // move the primes into the result
-        for (i = 0, j = 0; i < s; i++) {
-            if (f[i]) {// if prime
-                primes[j++] = i;
+        for (i = 0, j = 0; i < isCrossed.length; i++) {
+            if (isCrossed[i]) {// if prime
+                result[j++] = i;
             }
         }
     }
