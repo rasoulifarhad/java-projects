e7463596af6e6f997a94f68627dfb940de02e3b8
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-25 08:42:30 +0330

    updated

--- code-kata-project/src/main/java/com/farhad/example/codekata/bowling/Game.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/bowling/Game.java
@@ -26,15 +26,18 @@ public class Game {
     private void adjustCurrentFrame(int pins) {
         if (firstThrowInFrame == true) {
             if (pins == 10) {// strike
-                itsCurrentFrame++;
+                advanceFrame();
             } else {
                 firstThrowInFrame = false;
             }
         } else {
             firstThrowInFrame = true;
-            itsCurrentFrame++;
+            advanceFrame();
         }
-        itsCurrentFrame = Math.min(11, itsCurrentFrame);
+    }
+
+    private void advanceFrame(){
+        itsCurrentFrame = Math.min(11, itsCurrentFrame + 1);
     }
 
 }
