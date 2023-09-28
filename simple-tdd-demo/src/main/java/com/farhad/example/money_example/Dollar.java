cfbf8f34320e5c553442b03d4c8c298af97a09a7
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-28 21:38:01 +0330

    updated

--- simple-tdd-demo/src/main/java/com/farhad/example/money_example/Dollar.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/money_example/Dollar.java
@@ -15,7 +15,7 @@ public class Dollar extends Money{
     
     @Override
     public boolean equals(Object obj) {
-        Money dollar = (Dollar) obj;
+        Money dollar = (Money) obj;
         return this.amount == dollar.amount;
     }
 
