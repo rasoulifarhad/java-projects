8621e691aa58d980ab9a3e7d55b72ec9ade435f6
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-28 21:05:54 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/money_example/MoneyTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/money_example/MoneyTest.java
@@ -28,9 +28,9 @@ public class MoneyTest {
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.getAmount());
+        assertEquals(new Dollar(10), product);
         product = five.times(3); 
-        assertEquals(15, product.getAmount());
+        assertEquals(new Dollar(15), product);
     }
 
     @Test
