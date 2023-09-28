8fb5d67477a000329c0c1e81a135786d921883cd
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-28 20:36:37 +0330

    Dollar side-effect?

--- simple-tdd-demo/src/test/java/com/farhad/example/money_example/MoneyTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/money_example/MoneyTest.java
@@ -23,7 +23,7 @@ public class MoneyTest {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
-        five.times(3);
+        five.times(3); 
         assertEquals(15, five.amount);
     }
 
