68a960fc9c2ed0d336f377047eadb771297ae9ac
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-28 19:54:12 +0330

    ttd cycle: refactoring

--- simple-tdd-demo/src/main/java/com/farhad/example/money_example/Dollar.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/money_example/Dollar.java
@@ -2,13 +2,15 @@ package com.farhad.example.money_example;
 
 public class Dollar {
 
-    public Integer amount;
+    public int amount;
 
     public Dollar(int amount) {
+        this.amount = amount;
     }
+    
 
     public void times(int multiplier) {
-        this.amount = 5 * 2;
+        this.amount *= multiplier;
     }
 
 }
