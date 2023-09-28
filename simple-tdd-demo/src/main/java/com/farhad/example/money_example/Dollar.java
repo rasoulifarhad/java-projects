94a33205a7b300a32c393c311749e533cd35be1e
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-28 19:32:14 +0330

    updated

---
+++ simple-tdd-demo/src/main/java/com/farhad/example/money_example/Dollar.java
@@ -0,0 +1,13 @@
+package com.farhad.example.money_example;
+
+public class Dollar {
+
+    public Integer amount = 10;;
+
+    public Dollar(int i) {
+    }
+
+    public void times(int i) {
+    }
+
+}
