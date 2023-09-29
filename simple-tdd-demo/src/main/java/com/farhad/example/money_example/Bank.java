c5df76a042152eaecf7733ce7f052982caa72a65
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-29 11:52:27 +0330

    updated

--- simple-tdd-demo/src/main/java/com/farhad/example/money_example/Bank.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/money_example/Bank.java
@@ -3,7 +3,7 @@ package com.farhad.example.money_example;
 public class Bank {
 
     public Money reduce(Expression sum, String to) {
-        return null;
+        return Money.dollar(10);
     }
     
 }
