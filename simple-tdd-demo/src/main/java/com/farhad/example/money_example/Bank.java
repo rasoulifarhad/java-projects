589b5ede8e682feb98eb3d82811f606651fceb85
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-30 16:29:36 +0330

    updated

--- simple-tdd-demo/src/main/java/com/farhad/example/money_example/Bank.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/money_example/Bank.java
@@ -3,12 +3,7 @@ package com.farhad.example.money_example;
 public class Bank {
 
     public Money reduce(Expression source, String to) {
-        if (source instanceof Money) {
-            Money money = (Money) source;
-            return money.reduce(to);
-        }
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
     
 }
--- simple-tdd-demo/src/main/java/com/farhad/example/money_example/Expression.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/money_example/Expression.java
@@ -1,5 +1,5 @@
 package com.farhad.example.money_example;
 
 public interface Expression {
-    
+    Money reduce(String to);
 }
