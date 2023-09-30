892d84b50fc9a48545d3c2645ea7803be0acbd01
farhad rasouli <rasouli.farhad@gmail.com>
2023-09-30 23:50:42 +0330

    updated

--- java-design-patterns-03/src/main/java/com/farhad/example/factory_method/pizastore/pizzastores/PizzaStore.java
+++ java-design-patterns-03/src/main/java/com/farhad/example/factory_method/pizastore/pizzastores/PizzaStore.java
@@ -2,6 +2,32 @@ package com.farhad.example.factory_method.pizastore.pizzastores;
 
 import com.farhad.example.factory_method.pizastore.pizzas.Pizza;
 
+// Factory Method: Both Abstract Factory and
+// I create objects – that’s our jobs. But I do it through
+// inheritance...
+// 
+// Abstract Factory: ...and I do it through object
+// composition.
+// 
+// Abstract Factory, often use factory methods to implement your concrete factories?
+// concrete factories often implement a factory method to create their products.
+
+// Abstract Factory: Thanks. Remember me, Abstract
+// Factory, and use me whenever you have families of
+// products you need to create and you want to make sure
+// your clients create products that belong together.
+// 
+// Factory Method: And I’m Factory Method; use me to
+// decouple your client code from the concrete classes you
+// need to instantiate, or if you don’t know ahead of time
+// all the concrete classes you are going to need. To use me,
+// just subclass me and implement my factory method!
+// 
+// The intent of Factory Method is to allow a class to defer instantiation to its 
+// subclasses.
+// 
+// The intent of Abstract Factory is to create families of related objects without having 
+// to depend on their concrete classes.
 public abstract class PizzaStore {
 
     public Pizza ordePizza(String type) {
