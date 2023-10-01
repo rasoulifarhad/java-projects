3e4b8a20423fa95711a61eaa3839751b234cd208
farhad rasouli <rasouli.farhad@gmail.com>
2023-10-01 19:36:04 +0330

    updated

---
+++ simple-tdd-demo/src/main/java/com/farhad/example/money_example/Pair.java
@@ -0,0 +1,12 @@
+package com.farhad.example.money_example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Pair {
+    
+    private String from;
+    private String to;
+}
