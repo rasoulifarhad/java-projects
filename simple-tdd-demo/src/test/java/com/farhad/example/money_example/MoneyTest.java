e870f4c31fa60e1ec83ffdaec29559a699bd7aff
farhad rasouli <rasouli.farhad@gmail.com>
2023-10-01 19:47:30 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/money_example/MoneyTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/money_example/MoneyTest.java
@@ -85,8 +85,8 @@ public class MoneyTest {
 
     @Test
     public void testMixedAddition() {
-        Expression fiveBucks = dollar(5);
-        Expression tenFrancs = franc(10);
+        Money fiveBucks = dollar(5);
+        Money tenFrancs = franc(10);
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
