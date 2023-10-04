e72a23d4ab54a6899d7fd70151b9e8458506356b
farhad rasouli <rasouli.farhad@gmail.com>
2023-10-04 20:08:09 +0330

    updated

--- ddd-example-tips/src/main/java/com/farhad/example/ddd_tips/education_domain/domain/model/Course.java
+++ ddd-example-tips/src/main/java/com/farhad/example/ddd_tips/education_domain/domain/model/Course.java
@@ -1,7 +1,11 @@
 package com.farhad.example.ddd_tips.education_domain.domain.model;
 
+import java.util.Set;
+
 public class Course {
+
     private CourseId courseId;
     private String courseName;
     private int capacity;
+    Set<Subscription> subscriptions;
 }
