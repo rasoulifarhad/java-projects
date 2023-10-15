37d19b7dcf582baa3f215506c6e6562e88215acf
Farhad rasouli <rasouli.farhad@gmail.com>
2023-10-16 02:00:23 +0330

    updated

--- java-design-patterns-03/src/main/java/com/farhad/example/creational_patterns_demo/incentive_module/Incentive.java
+++ java-design-patterns-03/src/main/java/com/farhad/example/creational_patterns_demo/incentive_module/Incentive.java
@@ -1,25 +1,29 @@
 package com.farhad.example.creational_patterns_demo.incentive_module;
 
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+
 public class Incentive {
+	// Connect to the database
+	// Retrieve the minimal required information of this employee and their KPI from the database.
+	// Depending on certain conditions, we may need additional information from the database.
+	// The incentive calculation algorithm varies based on department and experience.
+	// We might need to connect to different databases to obtain specific data.
+	// We might need to connect to third-party services to acquire certain data.
+	// Process all the parameters 
+	// Provide the current year's incentive to the client	
 	Amount getIncentiveOf(EmployId id){
-		// Connect to the database
-		// Retrieve the minimal required information of this employee and their KPI from the database.
-		// Depending on certain conditions, we may need additional information from the database.
-		// The incentive calculation algorithm varies based on department and experience.
-		// We might need to connect to different databases to obtain specific data.
-		// We might need to connect to third-party services to acquire certain data.
-		// Process all the parameters 
-		// Provide the current year's incentive to the client	
-		return  null;
+		return  getKPIListOf(id).stream()
+						.map(kpi -> kpi.getIncentiveOf(id))
+						.map(Amount::getValue)
+						.reduce(BigDecimal::add)
+						.map(value -> new Amount(value))
+						.orElse(new Amount(BigDecimal.ZERO));
 	}
 
-	private void connectTopDB(){
-
+	private List<KPI>  getKPIListOf(EmployId employId) {
+		return Collections.emptyList();
 	}
-
-	private void secretLogicOfIncentive(){
-
-	}
-
 	
 }
---
+++ java-design-patterns-03/src/main/java/com/farhad/example/creational_patterns_demo/incentive_module/KPI.java
@@ -0,0 +1,21 @@
+package com.farhad.example.creational_patterns_demo.incentive_module;
+
+import java.util.UUID;
+
+public class KPI {
+
+	private UUID id;
+	private void connectTopDB(){
+
+	}
+
+	private void secretLogicOfIncentive(){
+
+	}
+
+	Amount getIncentiveOf(EmployId id){
+		return  null;
+	}
+
+	
+}
