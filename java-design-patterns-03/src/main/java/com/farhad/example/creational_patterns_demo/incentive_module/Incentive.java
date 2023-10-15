323f1fc0c6d7815cc04787821570afcac58bee3d
Farhad rasouli <rasouli.farhad@gmail.com>
2023-10-16 01:47:08 +0330

    updated

---
+++ java-design-patterns-03/src/main/java/com/farhad/example/creational_patterns_demo/incentive_module/Incentive.java
@@ -0,0 +1,25 @@
+package com.farhad.example.creational_patterns_demo.incentive_module;
+
+public class Incentive {
+	Amount getIncentiveOf(EmployId id){
+		// Connect to the database
+		// Retrieve the minimal required information of this employee and their KPI from the database.
+		// Depending on certain conditions, we may need additional information from the database.
+		// The incentive calculation algorithm varies based on department and experience.
+		// We might need to connect to different databases to obtain specific data.
+		// We might need to connect to third-party services to acquire certain data.
+		// Process all the parameters 
+		// Provide the current year's incentive to the client	
+		return  null;
+	}
+
+	private void connectTopDB(){
+
+	}
+
+	private void secretLogicOfIncentive(){
+
+	}
+
+	
+}
--- java-design-patterns-03/src/main/java/com/farhad/example/creational_patterns_demo/incentive_module/Service.java
+++ java-design-patterns-03/src/main/java/com/farhad/example/creational_patterns_demo/incentive_module/Service.java
@@ -2,6 +2,14 @@ package com.farhad.example.creational_patterns_demo.incentive_module;
 
 public interface Service {
 	// determines the respective incentive for the employee as of the current date
+	// Connect to the database
+	// Retrieve the minimal required information of this employee and their KPI from the database.
+	// Depending on certain conditions, we may need additional information from the database.
+	// The incentive calculation algorithm varies based on department and experience.
+	// We might need to connect to different databases to obtain specific data.
+	// We might need to connect to third-party services to acquire certain data.
+	// Process all the parameters 
+	// Provide the current year's incentive to the client	
 	Amount getIncentiveOf(EmployId id);
 	Amount getKYCOf(EmployId id);
 }
