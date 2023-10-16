ec37cdcf04ae8063ea198142b5fd16785b70291f
Farhad rasouli <rasouli.farhad@gmail.com>
2023-10-16 04:21:58 +0330

    updated

--- java-design-patterns-03/src/main/java/com/farhad/example/creational_patterns_demo/incentive_module/Incentive.java
+++ java-design-patterns-03/src/main/java/com/farhad/example/creational_patterns_demo/incentive_module/Incentive.java
@@ -1,9 +1,5 @@
 package com.farhad.example.creational_patterns_demo.incentive_module;
 
-import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.List;
-
 public class Incentive {
 	// Connect to the database
 	// Retrieve the minimal required information of this employee and their KPI from the database.
@@ -18,18 +14,10 @@ public class Incentive {
     // 2. It then makes a call to each KPI class. While calling a specific KPI class, it checks the employee’s department and, based on that, creates an object of the appropriate `KPI+Dep` class and calls its method to calculate the incentive for that KPI.
     // 3. The calculated value is then passed back to the `Incentive` class. `Incentive` class will sum up incentive amount from each KPI and give that to Client.
 
-	public List<String> getKPIListOfEmployee() {
-		return Collections.emptyList();
-	}
 
+	
 	public Amount getIncentiveOfEmployee(){
-		List<String> kpis = getKPIListOfEmployee();
-		return kpis.stream()
-					.map(this::findIncentiveOfKpi)
-					.map(Amount::getValue)
-					.reduce(BigDecimal::add)
-					.map(value -> new Amount(value))
-					.orElse(new Amount(BigDecimal.ZERO));
+		return null;
 	}
 
 	Amount findIncentiveOfKpi(String type) {
