f7cd93a0debc7251c6336579e3758327d7fcab68
Farhad rasouli <rasouli.farhad@gmail.com>
2023-10-19 11:31:34 +0330

    updated

--- design-principles-02/src/test/java/com/farhad/example/design_principles02/test_design/domain/application/AccountServiceTest.java
+++ design-principles-02/src/test/java/com/farhad/example/design_principles02/test_design/domain/application/AccountServiceTest.java
@@ -1,8 +1,11 @@
 package com.farhad.example.design_principles02.test_design.domain.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import com.farhad.example.design_principles02.test_design.domain.adapter.persistence.FakeAccountRepository;
 import com.farhad.example.design_principles02.test_design.domain.application.impl.AccountServiceImpl;
@@ -23,6 +26,18 @@ public class AccountServiceTest {
 		assertThat(account.getBalance()).isEqualTo(100d);
 	}
 
+	@Test
+	void addingTransactionToAccountDelegatesToAccountInstanceWithMockito() {
+		//given
+		Account account = new Account();
+		AccountRepository accountRepository = mock(AccountRepository.class);
+		Mockito.when(accountRepository.getByName(any())).thenReturn(account);
+		AccountService accountService = new AccountServiceImpl(accountRepository);
+		//when
+		accountService.addTransactionToAccount("Training Account", 100d);
+		//then
+		assertThat(account.getBalance()).isEqualTo(100d);
+	}
 	@Test
 	void t() {
 		//given
