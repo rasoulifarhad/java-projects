2f7a431e260f165053e4f2987bf4fb7167d6b63c
Farhad rasouli <rasouli.farhad@gmail.com>
2023-10-19 21:59:55 +0330

    updated

--- design-principles-02/src/test/java/com/farhad/example/design_principles02/test_design/domain/application/AccountServiceTest.java
+++ design-principles-02/src/test/java/com/farhad/example/design_principles02/test_design/domain/application/AccountServiceTest.java
@@ -3,8 +3,11 @@ package com.farhad.example.design_principles02.test_design.domain.application;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +20,10 @@ import com.farhad.example.design_principles02.test_design.domain.model.Account;
 import com.farhad.example.design_principles02.test_design.domain.model.AccountRepository;
 import com.farhad.example.design_principles02.test_design.domain.model.DomainException;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 public class AccountServiceTest {
 	
 	private AccountRepository fakeAccountRepository;
@@ -28,12 +35,16 @@ public class AccountServiceTest {
 
 	private Account mockAccount;
 
+	private AccountServiceBuilder accountServiceBuilder;
+	  
+
 	@BeforeEach
 	public void setup() {
 		mockAccount = mock(Account.class);
 		mockAccountRepository = mock(AccountRepository.class);
 		accountService = new AccountServiceImpl(mockAccountRepository);
 		fakeAccountRepository = new FakeAccountRepository(new Account());
+		accountServiceBuilder = AccountServiceBuilder.of();
 	}
 
 	@Test
@@ -51,14 +62,14 @@ public class AccountServiceTest {
 	@Test
 	void addingTransactionToAccountDelegatesToAccountInstanceWithMockito() {
 		//given
-		Account account = new Account();
+		Account account = mock(Account.class);
 		AccountRepository mockAccountRepository = mock(AccountRepository.class);
 		when(mockAccountRepository.getByName("Trading Account")).thenReturn(account);
 		AccountService accountService = new AccountServiceImpl(mockAccountRepository);
 		//when
 		accountService.addTransactionToAccount("Trading Account", 100d);
 		//then
-		assertThat(account.getBalance()).isEqualTo(100d);
+		verify(account,times(1)).addTransaction(100d);
 	}
 
 	@Test
@@ -110,10 +121,58 @@ public class AccountServiceTest {
 	}
 
 	@Test
+	void addingTransactionToAccountDelegatesToAccountInstanceWithBuilder() {
+		//given
+		AccountService accountService = accountServiceBuilder
+						.withAccountCalled("Trading Account")
+						// .addTransactionOfValue(200d)
+						.build(); 
+		//when
+		accountService.addTransactionToAccount("Trading Account", 100d);
+		//then
+		verify(accountServiceBuilder.account, times(1)).addTransaction(eq(100d));
+	}
+
+	@Test
 	void t() {
 		//given
 		//when
 		//then
+	}
+
+	static class AccountServiceBuilder {
+		
+		private AccountService accountService;
+		private AccountRepository accountRepository;
+
+		@Getter
+		@Setter(value = AccessLevel.PRIVATE)
+		private Account account;
+
+		private AccountServiceBuilder() {
+			accountRepository = mock(AccountRepository.class);
+			accountService = new AccountServiceImpl(accountRepository);
+		}
+
+		public static AccountServiceBuilder of() {
+			return new AccountServiceBuilder();
+		}
+		public AccountServiceBuilder withAccountCalled(String accountName) {
+			account = mock(Account.class);
+			when(accountRepository.getByName(accountName)).thenReturn(account);
+			return this;
+		}
+
+		public AccountServiceBuilder addTransactionOfValue(double amount) {
+			account.addTransaction(amount);
+			return this;
+		}
+
+		public AccountService build() {
+			return accountService;
+		}
+		
+
 	}
 
 }
