8950575cf6ac1d0c27e71e4c43d038ee88f7109a
Farhad rasouli <rasouli.farhad@gmail.com>
2024-01-06 06:53:45 +0330

    updated

--- java-multi-threading/src/main/java/com/farhad/example/cas/non_blocking_stack/CASStack.java
+++ java-multi-threading/src/main/java/com/farhad/example/cas/non_blocking_stack/CASStack.java
@@ -2,14 +2,14 @@ package com.farhad.example.cas.non_blocking_stack;
 
 import java.util.concurrent.atomic.AtomicReference;
 
-public class CASStack {
+public class CASStack<T> {
 
-	private AtomicReference<Node<Integer>> top = new AtomicReference<>();
+	private AtomicReference<Node<T>> top = new AtomicReference<>();
 
 
-	public void push(Node<Integer> newNode) {
+	public void push(Node<T> newNode) {
 		while (true) {
-			Node<Integer> currentTop = top.get();
+			Node<T> currentTop = top.get();
 			newNode.next = currentTop;
 			if(top.compareAndSet(currentTop, newNode)) {
 				break;
@@ -17,14 +17,14 @@ public class CASStack {
 		}
 	}
 
-	public Node<Integer> pop() {
+	public Node<T> pop() {
 		while (true) {
-			Node<Integer> currentTop = top.get();
+			Node<T> currentTop = top.get();
 			if (currentTop == null) {
 				return null;
 			}
 			
-			Node<Integer> newTop = currentTop.next;
+			Node<T> newTop = currentTop.next;
 			if(top.compareAndSet(currentTop, newTop)) {
 				return currentTop;
 			}
--- java-multi-threading/src/test/java/com/farhad/example/cas/non_blocking_stack/CASStackTest.java
+++ java-multi-threading/src/test/java/com/farhad/example/cas/non_blocking_stack/CASStackTest.java
@@ -1,4 +1,35 @@
 package com.farhad.example.cas.non_blocking_stack;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CASStackTest {
+
+	private CASStack<Integer> stack;
+
+	@BeforeEach
+	public void setup() {
+		stack = new CASStack<Integer>();
+	}
+
+	@AfterEach
+	public void teardown() {
+		stack = null;
+	}
+
+	@Test
+	public void addToStackTest() {
+		Node<Integer> newNode10 = new Node<Integer>(Integer.valueOf(10));
+		stack.push(newNode10);
+
+		Node<Integer> newNode9 = new Node<Integer>(Integer.valueOf(9));
+		stack.push(newNode9);
+
+	 	Node<Integer> popNode =  stack.pop();
+		assertTrue(newNode9.equals(popNode));
+
+	}
 }
