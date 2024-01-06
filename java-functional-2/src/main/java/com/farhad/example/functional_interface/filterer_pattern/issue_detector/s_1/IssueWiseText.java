11c49dfced4b7651878689539f34053e0f5b3a88
Farhad rasouli <rasouli.farhad@gmail.com>
2024-01-06 16:37:19 +0330

    updated

---
+++ java-functional-2/src/main/java/com/farhad/example/functional_interface/filterer_pattern/issue_detector/s_1/IssueWiseText.java
@@ -0,0 +1,10 @@
+package com.farhad.example.functional_interface.filterer_pattern.issue_detector.s_1;
+
+import java.util.List;
+
+public interface IssueWiseText {
+
+	String text();
+
+	List<Issue> issues();
+}
