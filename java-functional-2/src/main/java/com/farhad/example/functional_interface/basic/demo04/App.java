65604e9ba25f7a9b12240c6c8f2b78c85d7f48f3
Farhad rasouli <rasouli.farhad@gmail.com>
2024-01-22 13:01:37 +0330

    updated

--- java-functional-2/src/main/java/com/farhad/example/functional_interface/basic/demo04/App.java
+++ java-functional-2/src/main/java/com/farhad/example/functional_interface/basic/demo04/App.java
@@ -1,9 +1,15 @@
 package com.farhad.example.functional_interface.basic.demo04;
 
+import java.util.function.Function;
+
 public class App {
 	public static void main(String[] args) {
 		
 		System.out.println(Multiply.DIRECTMUL.apply(2, 3));
 		System.out.println(Multiply.OTHERMUL.apply(2, 3));
+
+		Function<Integer, Integer> plus1 = i -> i + 1;
+		System.out.println(Multiply.DIRECTMUL.andThen(plus1).apply(2, 3));
+		
 	}
 }
