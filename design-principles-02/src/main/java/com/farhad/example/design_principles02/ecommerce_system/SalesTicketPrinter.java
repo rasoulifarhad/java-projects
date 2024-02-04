d603c22d7bf0f5a6da326c139bda3679a8066221
Farhad rasouli <rasouli.farhad@gmail.com>
2024-02-04 09:36:00 +0330

    updated

--- design-principles-02/src/main/java/com/farhad/example/design_principles02/ecommerce_system/SalesTicketPrinter.java
+++ design-principles-02/src/main/java/com/farhad/example/design_principles02/ecommerce_system/SalesTicketPrinter.java
@@ -1,5 +1,16 @@
 package com.farhad.example.design_principles02.ecommerce_system;
 
+import lombok.Data;
+
+@Data
 public class SalesTicketPrinter {
 
+	private Header header;
+	private Footer footer;
+
+	private boolean printHeader = false;
+	private boolean printFooter = false;
+	public void printTicket() {
+		
+	}
 }
