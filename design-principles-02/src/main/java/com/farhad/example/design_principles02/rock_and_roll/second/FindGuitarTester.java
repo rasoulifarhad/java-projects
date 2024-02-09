0db3a107162b6f9cba8eeb8417b641b2fa6f2104
Farhad rasouli <rasouli.farhad@gmail.com>
2024-02-09 14:50:11 +0330

    updated

--- design-principles-02/src/main/java/com/farhad/example/design_principles02/rock_and_roll/second/FindGuitarTester.java
+++ design-principles-02/src/main/java/com/farhad/example/design_principles02/rock_and_roll/second/FindGuitarTester.java
@@ -13,8 +13,8 @@ public class FindGuitarTester {
 				Wood.ALDER);
 		List<Guitar> machedGuitar = inventory.search(whatBuyerLikes);
 
-		if (machedGuitar.size() > 0) {
-			System.out.println("Buyer, you might like this: ");
+		if (!machedGuitar.isEmpty()) {
+			System.out.println("Buyer, you might like these guitars: ");
 			System.out.println(machedGuitar);
 		} else {
 			System.out.println("Sorry, Buyer, we have nothing for you.");
