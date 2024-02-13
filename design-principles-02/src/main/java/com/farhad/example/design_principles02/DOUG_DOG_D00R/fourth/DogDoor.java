9a3e1cc3292cbcdfc50a8a73c0ece8f9f42de9d2
Farhad rasouli <rasouli.farhad@gmail.com>
2024-02-13 08:11:06 +0330

    updated

---
+++ design-principles-02/src/main/java/com/farhad/example/design_principles02/DOUG_DOG_D00R/fourth/DogDoor.java
@@ -0,0 +1,46 @@
+package com.farhad.example.design_principles02.DOUG_DOG_D00R.fourth;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class DogDoor {
+
+	private boolean open;
+
+	@Getter
+	@Setter
+	private String AllowedBark;
+
+	public DogDoor() {
+		this.open = false;
+	}
+
+	public void open() {
+		System.out.println("The dog door opens.");
+		this.open = true;
+
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				close();
+				timer.cancel();
+			}
+			
+		},5000);
+	}
+
+	public void close() {
+		System.out.println("The dog door closes");
+		this.open = false;
+	}
+
+	public boolean isOpen() {
+		return open;	
+	}
+	
+}
--- design-principles-02/src/main/java/com/farhad/example/design_principles02/DOUG_DOG_D00R/fourth/DogDoorSimulator.java
+++ design-principles-02/src/main/java/com/farhad/example/design_principles02/DOUG_DOG_D00R/fourth/DogDoorSimulator.java
@@ -1,32 +1,32 @@
-package com.farhad.example.design_principles02.DOUG_DOG_D00R.third;
+package com.farhad.example.design_principles02.DOUG_DOG_D00R.fourth;
 
 //
-// 1. Dog barks to be let out
-// 2. The bark recognizer hears a bark.
+// 1. The owner's Dog barks to be let out
+// 2. The bark recognizer hears a bark
 //
-//    2.1 She/He hears dog barking.
+//    2.1 The owner hears her dog barking.
 //
-// 3. The bark recognizer sends a requestto the door to open
+// 3. If it's the owner's dog barking, the bark recognizer sends a requestto the door to open
 //
-//    3.1 She/He presses the button on the remote control. 
+//    3.1 The owner presses  the button on the remote control.
 //
 // 4. The dog door opens.
-// 5. Dog goes outside.
-// 6. Dog does his business.
+// 5. The owner's Dog goes outside.
+// 6. The owner's Dog does his business.
 //
 //    6.1. The door shuts automatically.
-//    6.2. Dog barks to be let back inside.
+//    6.2. The owner's Dog barks to be let back inside.
 //    6.3. She/He hears dog barking (again).
 //
-//         6.3.1 The bark recognizer hears a bark (again).
+//         6.3.1 The owner hears her dog barking (again).
 //
-//    6.4. She/He presses the button on the remote control
+//    6.4. if it's the owner's dog barking, the bark recognizer send a request to the door  to open.
 //
-//         6.4.1 The bark recognizer sends a requestto the door to open
+//         6.4.1 The owner presses the buttun on the remote control.
 //
 //    6.5. The dog door opens (again).
 //
-// 7. Dog goes back inside.
+// 7. The owner's Dog goes back inside.
 // 8. The door shuts automatically.
 
 public class DogDoorSimulator {
