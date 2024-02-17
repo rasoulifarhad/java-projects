02eee7a9a047253c1bc623fe721f205f02b8a097
Farhad rasouli <rasouli.farhad@gmail.com>
2024-02-17 21:25:46 +0330

    updated

---
+++ design-principles-02/src/test/java/com/farhad/example/design_principles02/successive_refinement/ComparisonCompactorTest.java
@@ -0,0 +1,11 @@
+package com.farhad.example.design_principles02.successive_refinement;
+
+import org.junit.jupiter.api.Test;
+
+public class ComparisonCompactorTest {
+
+	@Test
+	public void testMessage() {
+		
+	}
+}
