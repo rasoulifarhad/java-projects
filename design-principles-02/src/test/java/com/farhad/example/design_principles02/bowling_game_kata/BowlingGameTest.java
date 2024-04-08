2c047fc5e7ea4d550f053cb7ac8200241f92125a
Farhad rasouli <rasouli.farhad@gmail.com>
2024-04-08 22:01:10 +0330

    updated

--- design-principles-02/src/test/java/com/farhad/example/design_principles02/bowling_game_kata/BowlingGameTest.java
+++ design-principles-02/src/test/java/com/farhad/example/design_principles02/bowling_game_kata/BowlingGameTest.java
@@ -28,9 +28,7 @@ public class BowlingGameTest {
 
 	@Test
 	public void testAllOnes() {
-		for (int i = 0; i < 20; i++) {
-			g.roll(1);
-		}
+		rollMany(20, 1);
 		assertEquals(20, g.score());
 
 	}
