1d42afdbd07fec034da76f3f344da4695d52bc45
Farhad rasouli <rasouli.farhad@gmail.com>
2024-04-08 22:01:49 +0330

    updated

--- design-principles-02/src/test/java/com/farhad/example/design_principles02/bowling_game_kata/BowlingGameTest.java
+++ design-principles-02/src/test/java/com/farhad/example/design_principles02/bowling_game_kata/BowlingGameTest.java
@@ -14,16 +14,16 @@ public class BowlingGameTest {
 		g = new Game();
 	}
 
-	@Test
-	public void testGutterGame() {
-		rollMany(20, 0);
-		assertEquals(0, g.score());
-	}
-
 	private void rollMany(int n, int pins) {
 		for (int i = 0; i < n; i++) {
 			g.roll(pins);
 		}
+	}
+
+	@Test
+	public void testGutterGame() {
+		rollMany(20, 0);
+		assertEquals(0, g.score());
 	}
 
 	@Test
