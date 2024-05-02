d8c48e458d3a7ff599f71b98a3f726c2c8a41f65
farhad rasouli <rasouli.farhad@gmail.com>
2024-05-02 16:23:28 +0330

    updated

--- simple-tdd-demo/src/main/java/com/farhad/example/tdd/Test.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/tdd/Test.java
@@ -4,10 +4,24 @@ public class Test {
 
     public static void main(String[] args) {
 
+        multiplication_ShouldResultInAMultiplicationOfTwoPassedNumbers();
+    }
+
+    private static void multiplication_ShouldResultInAMultiplicationOfTwoPassedNumbers() {
+        
+        // Assuming...
         Multiplication multiplication = new Multiplication(3, 7);
+        
+        // when this happens:
         int result = multiplication.perform();
-        if (result != 21 ) {
-            throw new RuntimeException("Failed! Expected 21 but was: " + result);
+        
+        // then the result should be...
+        assertTwoIntegersAreEqual(21, result);
+    }
+
+    private static void assertTwoIntegersAreEqual(int expected, int actual) {
+        if (actual != expected ) {
+            throw new RuntimeException("Failed! Expected: " + expected + "  but was: " + actual);
         }
     }
 }
