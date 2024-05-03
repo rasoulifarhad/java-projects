fb647194f818cb6088e11759517be126ea5e99c0
farhad rasouli <rasouli.farhad@gmail.com>
2024-05-03 16:45:29 +0330

    updated

--- simple-tdd-demo/src/main/java/com/farhad/example/parse_query_string/QueryString.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/parse_query_string/QueryString.java
@@ -5,20 +5,18 @@ import java.util.Map;
 
 public class QueryString {
 
-    private String query;
     Map<String, String> map = new HashMap<>();
 
     public QueryString(String queryString) {
         if(queryString == null) {
             throw new NullPointerException();
         }
-        this.query = queryString;
         parseQueryString(queryString);
     }
 
     private void parseQueryString(String queryString) {
         if(!"".equals(queryString)) {
-            String [] pairs = query.split("&");
+            String [] pairs = queryString.split("&");
             for (String pair : pairs) {
                 String [] nameValue = pair.split("=");
                 map.put(nameValue[0], nameValue[1]);
@@ -27,20 +25,10 @@ public class QueryString {
     }
 
     public Integer count() {
-        if("".equals(query)) {
-            return 0;
-        }
-        String [] pairs = query.split("&");
-        return pairs.length;
+        return map.size();
     }
 
     public Object valueFor(String name) {
-        
-        String [] pairs = query.split("&");
-        for (String pair : pairs) {
-            String [] nameValue = pair.split("=");
-            map.put(nameValue[0], nameValue[1]);
-        }
         return map.get(name);
     }
 
