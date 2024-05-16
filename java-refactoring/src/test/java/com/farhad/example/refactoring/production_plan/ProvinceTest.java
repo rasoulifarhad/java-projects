08b5eb1ab5d7899c1aab386170b765f086a52f9b
farhad rasouli <rasouli.farhad@gmail.com>
2024-05-16 23:55:41 +0330

    updated

--- java-refactoring/src/test/java/com/farhad/example/refactoring/production_plan/ProvinceTest.java
+++ java-refactoring/src/test/java/com/farhad/example/refactoring/production_plan/ProvinceTest.java
@@ -2,19 +2,26 @@ package com.farhad.example.refactoring.production_plan;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ProvinceTest {
 
+    private Province province;
+
+    @BeforeEach
+    public void setup() {
+        province = new Province(SampleProvinceData.get());
+    }
+    
     @Test
     public void shortfallTest() {
-        Province province = new Province(SampleProvinceData.get());
         assertEquals(province.getShortfall(), 5);
     }
 
+    
     @Test
     public void profitTest() {
-        Province province = new Province(SampleProvinceData.get());
         assertEquals(province.getProfit(), 230);
     }
 }
