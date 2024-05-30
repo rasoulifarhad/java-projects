23f95ec687e68586dcd1e1e0636dcde388e88b72
farhad rasouli <rasouli.farhad@gmail.com>
2024-05-30 23:30:17 +0330

    updated

--- java-refactoring/src/main/java/com/farhad/example/refactoring/roman_to_arabic/RomanNumeralsConverter.java
+++ java-refactoring/src/main/java/com/farhad/example/refactoring/roman_to_arabic/RomanNumeralsConverter.java
@@ -3,22 +3,20 @@ package com.farhad.example.refactoring.roman_to_arabic;
 public class RomanNumeralsConverter {
 
     public static int convert(String roman) {
-        if (roman.equals("I")){
-            return 1;
+        int sum = 0;
+        for (char ch : roman.toCharArray()) {
+            if(ch == 'I'){
+                sum++;
+            } else if(ch == 'V') {
+                sum += 5;
+            } else if(ch == 'X') {
+                sum += 10;
+            } else {
+                throw new IllegalArgumentException();
+            }
         }
-        if (roman.equals("II")){
-            return 2;
-        }
-        if (roman.equals("III")){
-            return 3;
-        }
-        if (roman.equals("V")) {
-            return 5;
-        }
-        if (roman.equals("X")) {
-                return 10;
-        }
-        throw new IllegalArgumentException();
+
+        return sum;
     }
 
 
