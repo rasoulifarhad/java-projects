f1df02bc5bff8e42cce0082832618f8c63780d09
farhad rasouli <rasouli.farhad@gmail.com>
2024-05-30 22:40:26 +0330

    updated

---
+++ java-refactoring/src/main/java/com/farhad/example/refactoring/roman_to_arabic/note.md
@@ -0,0 +1,1 @@
+As a schoolteacher, I want a tool that converts Roman numerals to Arabic numerals, so my students can check their homework grades quickly.
---
+++ java-refactoring/src/test/java/com/farhad/example/refactoring/roman_to_arabic/RomanNumeralsConverterTest.java
@@ -0,0 +1,13 @@
+package com.farhad.example.refactoring.roman_to_arabic;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class RomanNumeralsConverterTest {
+
+    @Test
+    public void isJunitWorking() {
+        assertTrue(true);
+    }
+}
