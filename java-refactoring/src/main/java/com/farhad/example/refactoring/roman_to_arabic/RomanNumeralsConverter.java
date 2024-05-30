7a525b73542e2f9683de55e748f10947a6809439
farhad rasouli <rasouli.farhad@gmail.com>
2024-05-30 23:38:54 +0330

    updated

--- java-refactoring/src/main/java/com/farhad/example/refactoring/roman_to_arabic/RomanNumeralsConverter.java
+++ java-refactoring/src/main/java/com/farhad/example/refactoring/roman_to_arabic/RomanNumeralsConverter.java
@@ -1,16 +1,24 @@
 package com.farhad.example.refactoring.roman_to_arabic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumeralsConverter {
+
+
+    private static final Map<Character, Integer> romanSymbols =  new HashMap<Character, Integer>(){
+        {
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+        }
+    };
 
     public static int convert(String roman) {
         int sum = 0;
         for (char ch : roman.toCharArray()) {
-            if(ch == 'I'){
-                sum++;
-            } else if(ch == 'V') {
-                sum += 5;
-            } else if(ch == 'X') {
-                sum += 10;
+            if(romanSymbols.containsKey(ch)) {
+                sum += romanSymbols.get(ch);
             } else {
                 throw new IllegalArgumentException(String.format("Illegal roman character %s", ch));
             }
