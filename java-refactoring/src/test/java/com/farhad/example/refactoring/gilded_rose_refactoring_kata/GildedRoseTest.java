6e722fdc5b01d97f111dda39f7c179b33356abfe
farhad rasouli <rasouli.farhad@gmail.com>
2024-05-31 03:42:42 +0330

    updated

--- java-refactoring/src/test/java/com/farhad/example/refactoring/gilded_rose_refactoring_kata/GildedRoseTest.java
+++ java-refactoring/src/test/java/com/farhad/example/refactoring/gilded_rose_refactoring_kata/GildedRoseTest.java
@@ -8,38 +8,36 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        Item item = createAndUpdate(0, 0);
+        assertEquals("foo", item.name);
     }
 
     @Test
     public void systemLowersValues() {
 
-        Item[] items = new Item[] {new Item("foo", 15, 25)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(14, app.items[0].sellIn);
-        assertEquals(24, app.items[0].quality);
+        Item item = createAndUpdate(15, 25);
+        assertEquals(14, item.sellIn);
+        assertEquals(24, item.quality);
     }
 
     @Test
     public void qualityDegradesTwiceAsFast() {
-        Item[] items = new Item[] {new Item("foo", 0, 17)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(15, app.items[0].quality);
+        Item item = createAndUpdate(0, 17);
+        assertEquals(15, item.quality);
     }
 
     @Test
     public void qualityIsNeverNegative() {
-        Item[] items = new Item[] {new Item("foo", 0, 0)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(0, app.items[0].quality);
+        Item item = createAndUpdate(0, 0);
+        assertEquals(0, item.quality);
 
     }
-    
+
+    private Item createAndUpdate(int sellIn, int quality) {
+        Item[] items = new Item[] { new Item("foo", sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return app.items[0];
+    }
 
 }
