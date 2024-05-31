9ff02735b406eea164dfc734c24189cc0e86079f
farhad rasouli <rasouli.farhad@gmail.com>
2024-05-31 03:33:18 +0330

    updated

--- java-refactoring/src/test/java/com/farhad/example/refactoring/gilded_rose_refactoring_kata/GildedRoseTest.java
+++ java-refactoring/src/test/java/com/farhad/example/refactoring/gilded_rose_refactoring_kata/GildedRoseTest.java
@@ -14,4 +14,32 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    public void systemLowersValues() {
+
+        Item[] items = new Item[] {new Item("foo", 15, 25)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(14, app.items[0].sellIn);
+        assertEquals(24, app.items[0].quality);
+    }
+
+    @Test
+    public void qualityDegradesTwiceAsFast() {
+        Item[] items = new Item[] {new Item("foo", 0, 17)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(15, app.items[0].quality);
+    }
+
+    @Test
+    public void qualityIsNeverNegative() {
+        Item[] items = new Item[] {new Item("foo", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+
+    }
+    
+
 }
