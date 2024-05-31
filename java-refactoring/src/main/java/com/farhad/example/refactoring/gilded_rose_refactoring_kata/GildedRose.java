9355e24e858d9470f8a257f805597f9bc3a8f169
farhad rasouli <rasouli.farhad@gmail.com>
2024-05-31 20:00:38 +0330

    updated

--- java-refactoring/src/main/java/com/farhad/example/refactoring/gilded_rose_refactoring_kata/GildedRose.java
+++ java-refactoring/src/main/java/com/farhad/example/refactoring/gilded_rose_refactoring_kata/GildedRose.java
@@ -19,8 +19,7 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            if (!isAgedBrie(item)
-                    && !isBackstagePasses(item)) {
+            if (!( isAgedBrie(item) || isBackstagePasses(item)) ) {
                 if (item.quality > 0) {
                     if (!isSulfuras(item)) {
                         item.quality--;
