af88178f8e3dc69b35c0ee2edca9bfd029dec615
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-03 18:28:25 +0330

    Once I’ve made the change for all methods of department and for all the clients of person, I can remove the department accessor on person.

--- java-refactoring/src/main/java/com/farhad/example/refactoring/hide_delegate/demo2/App.java
+++ java-refactoring/src/main/java/com/farhad/example/refactoring/hide_delegate/demo2/App.java
@@ -5,8 +5,7 @@ public class App {
     public static void main(String[] args) {
         Manager aManager = new Manager("manager name");
         Department aDepartment = new Department("charge code", aManager);
-        Person aPerson = new Person("person name");
-        aPerson.setDepartment(aDepartment);
+        Person aPerson = new Person("person name", aDepartment);
         Manager manager = aPerson.getManager();
         System.out.println(manager);
     }
--- java-refactoring/src/main/java/com/farhad/example/refactoring/hide_delegate/demo2/Person.java
+++ java-refactoring/src/main/java/com/farhad/example/refactoring/hide_delegate/demo2/Person.java
@@ -2,17 +2,13 @@ package com.farhad.example.refactoring.hide_delegate.demo2;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @RequiredArgsConstructor
 public class Person {
 
     @Getter
     private final String name;
-
-    @Getter
-    @Setter
-    private Department department;
+    private final Department department;
 
     public Manager getManager() {
         return department.getManager();
