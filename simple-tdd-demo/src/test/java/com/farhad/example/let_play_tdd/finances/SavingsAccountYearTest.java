581f7d3c477d6dc7a2b412d276dbee8fed665efc
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-04 21:21:11 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/let_play_tdd/finances/SavingsAccountYearTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/let_play_tdd/finances/SavingsAccountYearTest.java
@@ -8,31 +8,37 @@ public class SavingsAccountYearTest {
  
     @Test
     public void startingBalance() {
-        SavingsAccountYear account = new SavingsAccountYear(10_000, 10);
+        SavingsAccountYear account = createAccount();
         assertEquals(10_000, account.startingBalance());
-    } 
+    }
 
     @Test
     public void interestRate() {
-        SavingsAccountYear account = new SavingsAccountYear(10_000, 10);
+        SavingsAccountYear account = createAccount();
         assertEquals(10, account.interestRate());
     }
 
     @Test
     public void endingBalance() {
-        SavingsAccountYear account = new SavingsAccountYear(10_000, 10);
+        SavingsAccountYear account = createAccount();
         assertEquals(11_000, account.endingBalance());
     }
 
     @Test
     public void nextYearStartingBalanceShouldEqualThisYearEndingBalance() {
-        SavingsAccountYear thisYear = new SavingsAccountYear(10_000, 10);
+        SavingsAccountYear thisYear = createAccount();
         assertEquals(thisYear.endingBalance(), thisYear.nextYear().startingBalance());
     }
 
     @Test
     public void nextYearInterestingRateEqualsThisYearIntrestingYear() {
-        SavingsAccountYear thisYear = new SavingsAccountYear(10_000, 10);
+        SavingsAccountYear thisYear = createAccount();
         assertEquals(thisYear.interestRate(), thisYear.nextYear().interestRate());
     }
+
+    private SavingsAccountYear createAccount() {
+        SavingsAccountYear account = new SavingsAccountYear(10_000, 10);
+        return account;
+    } 
+
 }
