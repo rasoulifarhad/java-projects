804a1f4137465229081254f7146c42f44fd00fdc
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-04 13:32:42 +0330

    updated

---
+++ java-refactoring/src/main/java/com/farhad/example/refactoring/split_loop/demo1/Person.java
@@ -0,0 +1,10 @@
+package com.farhad.example.refactoring.split_loop.demo1;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private int age;
+    private double salary;
+}
---
+++ java-refactoring/src/main/java/com/farhad/example/refactoring/split_loop/demo1/Summary.java
@@ -0,0 +1,12 @@
+package com.farhad.example.refactoring.split_loop.demo1;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor
+public class Summary {
+
+    private int youngestAge;
+    private double totalSammary;
+}
