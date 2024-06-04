d8a1d994918d09c2216b9ddbe64082dac1e3f73c
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-04 04:58:08 +0330

    updated

--- java-refactoring/src/main/java/com/farhad/example/refactoring/move_function/demo1/GPSTrack.java
+++ java-refactoring/src/main/java/com/farhad/example/refactoring/move_function/demo1/GPSTrack.java
@@ -1,28 +1,30 @@
 package com.farhad.example.refactoring.move_function.demo1;
 
+import java.util.function.Supplier;
+
 public class GPSTrack {
 
 
     public TrackSummary trackSummary(Point[] points) {
-        int totalTime=  calculateTime();
-        int totalDistance = calculateDistance(points);
+        Supplier<Integer> calculateDistance = () -> {
+            int result = 0;
+            for (int i = 0; i < points.length; i++) {
+                result += distance(points[i-1], points[i]);
+            }
+           return result;
+        };
+
+        Supplier<Integer> calculateTime = () -> {
+            return 0;
+        };
+        int totalTime =  calculateTime.get();
+        int totalDistance = calculateDistance.get();
         int pace = totalTime / 60 / totalDistance;
         return new TrackSummary(totalTime, totalDistance, pace);
-    }
-
-    private int calculateDistance(Point[] points) {
-        int result = 0;
-        for (int i = 0; i < points.length; i++) {
-            result += distance(points[i-1], points[i]);
-        }
-        return result;
     }
 
     private int distance(Point point, Point point2) {
         return 0;
     }
 
-    private int calculateTime() {
-        return 0;
-    }
 }
