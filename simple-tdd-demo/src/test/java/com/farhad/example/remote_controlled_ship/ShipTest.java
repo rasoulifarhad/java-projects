45db094cbc379b54a8fd70c3f5bf29df22347079
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-09 23:43:19 +0330

    UPDATED

--- simple-tdd-demo/src/test/java/com/farhad/example/remote_controlled_ship/ShipTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/remote_controlled_ship/ShipTest.java
@@ -2,16 +2,23 @@ package com.farhad.example.remote_controlled_ship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ShipTest {
 
+    private Ship ship;
+    private Location location;
+
+    @BeforeEach
+    public void setup() {
+
+        location = new Location(new Point(21, 13), Direction.NORTH);
+        ship = new Ship(location);
+    }
+
     @Test
     public void whenInstantiatedThenLocationIsSet() {
-        Location location = new Location(
-            new Point(21, 13), 
-            Direction.NORTH);
-        Ship ship = new Ship(location);
         assertEquals(location, ship.getLocation());
     }
 }
