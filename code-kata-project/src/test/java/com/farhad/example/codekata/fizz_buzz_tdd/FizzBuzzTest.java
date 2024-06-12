2252f8af5c4c9e5228447a2284f37b755c1ce54b
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-13 01:57:32 +0330

    updated

--- code-kata-project/src/test/java/com/farhad/example/codekata/fizz_buzz_tdd/FizzBuzzTest.java
+++ code-kata-project/src/test/java/com/farhad/example/codekata/fizz_buzz_tdd/FizzBuzzTest.java
@@ -2,15 +2,21 @@ package com.farhad.example.codekata.fizz_buzz_tdd;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
 
+
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void givenIntegerOf1ShouldReturnStringOf1() {
-
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(1);
-        assertEquals("1", actual);
+        assertEquals("1", fizzBuzz.convert(1));
     }
 }
