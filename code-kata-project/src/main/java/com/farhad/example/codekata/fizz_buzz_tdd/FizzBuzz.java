31b213f124bb4f1341d31b5d6b401f0ea44838cf
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-13 03:13:48 +0330

    updated

--- code-kata-project/src/main/java/com/farhad/example/codekata/fizz_buzz_tdd/FizzBuzz.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/fizz_buzz_tdd/FizzBuzz.java
@@ -4,13 +4,13 @@ public class FizzBuzz {
 
     public String trasnsform(int source) {
         if(source == 1) {
-            return "1";        
+            return String.valueOf(source);        
         }
         if(source == 2) {
-            return "2";        
+            return String.valueOf(source);        
         }
         if(source == 3) {
-            return "3";        
+            return String.valueOf(source);        
         }
         throw new IllegalArgumentException("WE BROKE SOMETHING");
     }
