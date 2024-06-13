861273af9a0df7159c9900876a327f51145bb909
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-13 07:29:52 +0330

    updated

--- code-kata-project/src/main/java/com/farhad/example/codekata/fizz_buzz_chain/Appp.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/fizz_buzz_chain/Appp.java
@@ -8,15 +8,23 @@ public class Appp {
         IFizzBuzz b = new BuzzLink(f);
         IFizzBuzz fb = new FizzBuzzLink(b);
         FizzBuzz fizzBuzz = new FizzBuzz(fb);
-        System.out.println( fizzBuzz.convert(30));
-        System.out.println( fizzBuzz.convert(25));
-        System.out.println( fizzBuzz.convert(6));
-        System.out.println( fizzBuzz.convert(11));
+        System.out.println(fizzBuzz.convert(30));
+        System.out.println(fizzBuzz.convert(25));
+        System.out.println(fizzBuzz.convert(6));
+        System.out.println(fizzBuzz.convert(11));
 
         IFizzBuzz links = new FizzBuzzLink(
-            new BuzzLink(
-                new FizzLink(
-                    new ToStringLink())));
+                new BuzzLink(
+                        new FizzLink(
+                                new ToStringLink())));
         System.out.println(links.convert(25));
+
+        IFizzBuzz links2 = new FizzBuzz();
+        System.out.println();
+        System.out.println(links2.convert(30));
+        System.out.println(links2.convert(25));
+        System.out.println(links2.convert(6));
+        System.out.println(links2.convert(11));
+
     }
 }
--- code-kata-project/src/main/java/com/farhad/example/codekata/fizz_buzz_chain/FizzBuzz.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/fizz_buzz_chain/FizzBuzz.java
@@ -4,15 +4,19 @@ public class FizzBuzz implements IFizzBuzz {
 
     protected IFizzBuzz _next;
 
+    public FizzBuzz() {
+        this(new FizzBuzzLink(
+                new BuzzLink(
+                        new FizzLink(
+                                new ToStringLink()))));
+    }
 
     public FizzBuzz(IFizzBuzz fizzBuzz) {
         this._next = fizzBuzz;
     }
 
-
     public String convert(int input) {
         return _next.convert(input);
     }
 
-   
 }
