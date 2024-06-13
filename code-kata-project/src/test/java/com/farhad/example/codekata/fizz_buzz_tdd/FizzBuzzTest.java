3fef0b8137331f1c222f5002a128184a2e01ffe7
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-13 03:34:08 +0330

    updated

--- code-kata-project/src/test/java/com/farhad/example/codekata/fizz_buzz_tdd/FizzBuzzTest.java
+++ code-kata-project/src/test/java/com/farhad/example/codekata/fizz_buzz_tdd/FizzBuzzTest.java
@@ -20,40 +20,11 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void givenInt1ShouldReturnString1() {
-        int valueToTransform = 1;
-        String transformedValue = "1";
-        String actual = fizzBuzz.trasnsform(valueToTransform);
-        assertEquals(transformedValue, actual);
-    }
-
-    @Test
-    public void givenInt2ShouldReturnString2() {
-        int valueToTransform = 2;
-        String transformedValue = "2";
-        String actual = fizzBuzz.trasnsform(valueToTransform);
-        assertEquals(transformedValue, actual);
-    }
-
-    @Test
-    public void givenInt3ShouldReturnString3() {
-        int valueToTransform = 3;
-        String transformedValue = "3";
-        String actual = fizzBuzz.trasnsform(valueToTransform);
-        assertEquals(transformedValue, actual);
-    }
-
-    @Test
     public void givenInputReturnStringOfInput() {
         Map<Integer, String> regressionValues = new HashMap<Integer, String>() {
-            {
-                put(1,"1");
-                put(2,"2");
-                put(3,"3");
-            }
-        };
-
-        
+            {put(1,"1");
+             put(2,"2");
+             put(3,"3");}};
         int valueToTransform = 1 + new Random().nextInt(3);;
         String transformedValue = regressionValues.get(valueToTransform);
         String actual = fizzBuzz.trasnsform(valueToTransform);
