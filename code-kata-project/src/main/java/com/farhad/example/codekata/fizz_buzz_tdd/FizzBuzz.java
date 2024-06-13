5207fdedebe459cc26318a3ee954d25b4fc7daac
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-13 22:16:42 +0330

    updated

--- code-kata-project/src/main/java/com/farhad/example/codekata/fizz_buzz_tdd/FizzBuzz.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/fizz_buzz_tdd/FizzBuzz.java
@@ -3,6 +3,9 @@ package com.farhad.example.codekata.fizz_buzz_tdd;
 public class FizzBuzz {
 
     public String trasnsform(int source) {
+        if(source == 30) {
+            return "FizzBuzz";
+        }
         if(source == 15) {
             return "FizzBuzz";
         }
