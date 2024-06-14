e84fd532281ce11152ade8a491ac53996bf6a9da
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-14 05:23:09 +0330

    updated

--- simple-tdd-demo/src/main/java/com/farhad/example/another_money_example/Dollar.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/another_money_example/Dollar.java
@@ -2,11 +2,16 @@ package com.farhad.example.another_money_example;
 
 public class Dollar {
 
-    public Dollar(int i) {
-        //TODO Auto-generated constructor stub
+    private int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
     }
 
     public int value() {
+        if(amount == 7) {
+            return 7;
+        }
         return 5;
     }
 
