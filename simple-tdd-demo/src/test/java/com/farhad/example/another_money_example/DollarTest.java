9eb1580113dfc4c9eb2a3c88c41548e8d619b184
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-14 05:49:48 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/DollarTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/DollarTest.java
@@ -5,22 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Random;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class DollarTest {
 
 
-    @ParameterizedTest
-    @ValueSource(ints = {5, 7, 9})
-    public void savenDollarsHasValue(int input) {
-        Dollar subject = new Dollar(input);
-        int actual = subject.value();
-
-        assertEquals(input, actual);
-    }
-
-    @Test
+     @Test
     public void savenDollarsHasValue() {
         int value = 1 + new Random().nextInt(20);
         Dollar subject = new Dollar(value);
