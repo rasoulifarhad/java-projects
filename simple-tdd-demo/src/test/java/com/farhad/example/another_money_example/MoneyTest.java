bd1e0c753dc0a336330b103a2b2f277d7b19c7e4
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-14 19:32:01 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/MoneyTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/MoneyTest.java
@@ -10,15 +10,15 @@ public class MoneyTest {
 
     @Test
     public void asStringExists() {
-        Money money = new TestMoney(20, "CUR");
+        Money money = new Money(20, "CUR");
         String actual = money.asString();
         assertEquals("[amount=20.0][currency=CUR]", actual);
     }
 
     @Test
     public void fiveCurrencyPlus10CurrencyShouldBe15Currency() {
-        Money five = new TestMoney(5, "CUR");
-        Money ten = new TestMoney(10, "CUR");
+        Money five = new Money(5, "CUR");
+        Money ten = new Money(10, "CUR");
         Money actual = five.plus(ten);
         assertEquals("[amount=15.0][currency=CUR]", actual.asString());
     }
@@ -27,7 +27,7 @@ public class MoneyTest {
     public void moneyTimes() {
         double value = Double.valueOf(1 + new Random().nextInt(20));
         int multiplicand =  1 + new Random().nextInt(20);
-        TestMoney subject = new TestMoney(value, "CUR");
+        Money subject = new Money(value, "CUR");
         Money actual = subject.times(multiplicand);
         assertEquals(String.format("[amount=%s][currency=CUR]", value * multiplicand), actual.asString());
     }
@@ -36,17 +36,9 @@ public class MoneyTest {
     public void divideMoney() {
         double value = Double.valueOf(1 + new Random().nextInt(20));
         int divisor =  1 + new Random().nextInt(20);
-        TestMoney subject = new TestMoney(value, "CUR");
+        Money subject = new Money(value, "CUR");
         Money actual = subject.divide(divisor);
         assertEquals(String.format("[amount=%.1f][currency=CUR]", value / divisor), actual.asString());
-    }
-
-    private static class TestMoney extends Money {
-
-        protected TestMoney(double amount, String currency) {
-            super(amount, currency);
-        }
-
     }
 
 }
