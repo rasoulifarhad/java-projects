a63df1c7ee730007153c382b426a1313870a2596
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-14 03:43:32 +0330

    updated

--- simple-tdd-demo/src/main/java/com/farhad/example/another_money_example/Dollar.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/another_money_example/Dollar.java
@@ -2,4 +2,8 @@ package com.farhad.example.another_money_example;
 
 public class Dollar {
 
+    public Dollar(int i) {
+        //TODO Auto-generated constructor stub
+    }
+
 }
--- simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/DollarTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/DollarTest.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.Test;
 
 public class DollarTest {
 
-    @Test
-    public void dollarExists() {
-        new Dollar();
-    }
 
     @Test
     public void takeAmountInConstructor()  {
