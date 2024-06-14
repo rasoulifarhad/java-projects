f0003160d83b4c4746de7b0ace309be79e423782
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-14 17:39:16 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/MoneyTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/MoneyTest.java
@@ -28,16 +28,11 @@ public class MoneyTest {
 
     @Test
     public void divideExists() {
-        Money subject = new TestMoney(4002, "CUR");
-        double actual = subject.divide(4);
-        assertEquals(1000.5, actual);
-    }
-
-    @Test
-    public void divideExists2() {
-        Money subject = new TestMoney(10000, "CUR");
-        double actual = subject.divide(10);
-        assertEquals(1000, actual);
+        double value = Double.valueOf(1 + new Random().nextInt(20));
+        int divisor =  1 + new Random().nextInt(20);
+        TestMoney subject = new TestMoney(value, "USD");
+        double actual = subject.divide(divisor);
+        assertEquals(value / divisor, actual);
     }
 
     private static class TestMoney extends Money {
