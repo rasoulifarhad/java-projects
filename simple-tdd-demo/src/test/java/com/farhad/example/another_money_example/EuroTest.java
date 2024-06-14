4366c5c1972439c21fcca5f1693411982dbf65ec
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-14 13:49:53 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/EuroTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/EuroTest.java
@@ -1,5 +1,7 @@
 package com.farhad.example.another_money_example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class EuroTest {
@@ -8,6 +10,15 @@ public class EuroTest {
     @Test
     public void derivesFromMoney() {
         Money money = new Euro(20);
+    }
+
+        @Test
+    public void asStringReturnsValueAndCurrency() {
+
+        Money money = new Euro(20);
+        String actual = money.asString();
+
+        assertEquals("[amount=20][currency=EUR]", actual);
     }
 
 }
