5f0f3d6649d20c8b0d2d99284017b28d794a32ac
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-14 04:15:50 +0330

    updated

--- java-tips/src/main/java/com/farhad/example/java_tips/technical_practices/no_getters/Cash01.java
+++ java-tips/src/main/java/com/farhad/example/java_tips/technical_practices/no_getters/Cash01.java
@@ -11,4 +11,15 @@ public class Cash01 {
         return value;
     }
 
+    public static void main(String[] args) {
+        Cash01 costOfMeal = new Cash01(10);
+        Cash01 cash = new Cash01(20);
+
+        int amountToPay = costOfMeal.getValue();
+        int onHand = cash.getValue();
+        if(amountToPay <= onHand) {
+            System.out.println("You can pay");;
+        }
+    }
+
 }
