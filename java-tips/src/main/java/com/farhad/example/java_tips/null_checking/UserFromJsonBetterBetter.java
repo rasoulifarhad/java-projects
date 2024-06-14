091d0f291ea7b0ed926a139f39066ddab3b7a595
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-15 00:23:47 +0330

    updated

--- java-tips/src/main/java/com/farhad/example/java_tips/null_checking/UserFromJsonBetterBetter.java
+++ java-tips/src/main/java/com/farhad/example/java_tips/null_checking/UserFromJsonBetterBetter.java
@@ -2,6 +2,7 @@ package com.farhad.example.java_tips.null_checking;
 
 import com.farhad.example.java_tips.null_checking.our.IJsonParser;
 import com.farhad.example.java_tips.null_checking.our.IOurJObject;
+import com.farhad.example.java_tips.null_checking.our.JsonParserBetter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +13,11 @@ public class UserFromJsonBetterBetter  implements User {
     private final IJsonParser jParser;
     private String nameKey;
 
+
+    
+    public UserFromJsonBetterBetter(String json) {
+        this(json, new JsonParserBetter() );
+    }
 
     @Override
     public String greeting() {
