638dbbc10e484b03aebd87624d548d39a26d5c76
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-14 16:53:48 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/WonTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/WonTest.java
@@ -1,5 +1,7 @@
 package com.farhad.example.another_money_example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class WonTest {
@@ -9,5 +11,13 @@ public class WonTest {
         Money money = Money.won(20);
     }
 
+    @Test
+    public void asStringReturnsValueAndCurrency() {
+
+        Money money = Money.won(20);
+        String actual = money.asString();
+
+        assertEquals("[amount=20][currency=KRW]", actual);
+    }
 
 }
