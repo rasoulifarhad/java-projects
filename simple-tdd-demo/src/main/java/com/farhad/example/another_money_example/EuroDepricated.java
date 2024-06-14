2c23dd612f2620d914ac9a0b894d751a50f3f73a
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-14 14:37:53 +0330

    updated

--- simple-tdd-demo/src/main/java/com/farhad/example/another_money_example/EuroDepricated.java
+++
@@ -1,9 +0,0 @@
-package com.farhad.example.another_money_example;
-
-public class EuroDepricated extends Money {
-
-    public EuroDepricated(int amount) {
-        super(amount, "EUR");
-    }
-
-}
--- simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/EuroTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/EuroTest.java
@@ -8,20 +8,6 @@ public class EuroTest {
 
 
     @Test
-    public void derivesFromMoney() {
-        Money money = new EuroDepricated(20);
-    }
-
-    @Test
-    public void asStringReturnsValueAndCurrency() {
-
-        Money money = new EuroDepricated(20);
-        String actual = money.asString();
-
-        assertEquals("[amount=20][currency=EUR]", actual);
-    }
-
-    @Test
     public void derivesFromMoney2() {
         Money money = Money.euro(20);
     }
