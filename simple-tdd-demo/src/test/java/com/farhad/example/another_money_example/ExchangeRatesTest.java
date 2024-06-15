fcd28023052fc9ec5d01322526ac96d356deee0f
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-15 23:57:18 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/ExchangeRatesTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/ExchangeRatesTest.java
@@ -1,5 +1,6 @@
 package com.farhad.example.another_money_example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -15,5 +16,16 @@ public class ExchangeRatesTest {
             subject.exchangeRate("USD", "BLA_BLA_BLAAAAA");
         });
     }
+
+    @Test
+    public void fiveDollarsPlusTenEuroShouldBeSeventeenDollars() {
+        Money fiveDollar = Money.dollar(5);
+        Money tenEuro = Money.euro(10);
+
+        Money actual = fiveDollar.plus(tenEuro);
+
+        assertEquals(Money.dollar(17), actual);
+    }
+
 
 }
