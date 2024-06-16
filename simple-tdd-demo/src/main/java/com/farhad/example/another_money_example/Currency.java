5d092f05c0c2f517d884c2220b09593a881595c0
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-17 01:51:30 +0330

    updated

--- simple-tdd-demo/src/main/java/com/farhad/example/another_money_example/Currency.java
+++ simple-tdd-demo/src/main/java/com/farhad/example/another_money_example/Currency.java
@@ -2,6 +2,10 @@ package com.farhad.example.another_money_example;
 
 public class Currency {
 
+    public static Currency UsDollar = new Currency("USD");
+    public static Currency KoreanWon = new Currency("KRW");
+    public static Currency Euro = new Currency("EUR");
+
     private String currency;
 
     public Currency(String currency) {
