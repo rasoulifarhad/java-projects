aa0c305125dd2ac12b984460882b64b4c36dffcb
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-17 02:01:52 +0330

    updated

--- simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/CurrencyTest.java
+++ simple-tdd-demo/src/test/java/com/farhad/example/another_money_example/CurrencyTest.java
@@ -4,20 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.farhad.example.another_money_example.Currency.DefaultCurrency;
+
 public class CurrencyTest {
 
     @Test
     public void asStringReturnUSD() {
-        Currency subject = new Currency("USD");
-
+        Currency subject = DefaultCurrency.UsDollar;
         String actual = subject.asString();
 
         assertEquals("USD", actual);
     }
 
     @Test
-    public void ausdAsCurrency() {
-        Currency subject = Currency.UsDollar;
+    public void UsdAsCurrency()
+    {
+        Currency subject = DefaultCurrency.UsDollar;
 
         String actual = subject.asString();
 
