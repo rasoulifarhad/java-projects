1d1cadd279eec0267a42d10f3dd3d58e9d8952f2
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-18 01:59:37 +0330

    updated

---
+++ code-kata-project/src/test/java/com/farhad/example/codekata/event_proccessing_design_kata/option3/EventParserTest.java
@@ -0,0 +1,19 @@
+package com.farhad.example.codekata.event_proccessing_design_kata.option3;
+
+import java.io.InputStream;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import com.farhad.example.codekata.event_proccessing_design_kata.Spike;
+
+public class EventParserTest {
+
+    private InputStream inputStream;
+
+    @BeforeEach
+    public void setup() {
+        String filename = "com/farhad/example/codekata/event_proccessing_design_kata/sample_store.xml";
+        ClassLoader classLoader = Spike.class.getClassLoader();
+        inputStream = classLoader.getResourceAsStream(filename);
+    }
+}
