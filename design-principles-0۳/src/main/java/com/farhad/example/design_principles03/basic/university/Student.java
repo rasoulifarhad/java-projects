b550350729353deba44eb9736da8f3641d2f9abf
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-19 22:58:45 +0330

    updated

--- design-principles-0۳/src/main/java/com/farhad/example/design_principles03/basic/university/Student.java
+++ design-principles-0۳/src/main/java/com/farhad/example/design_principles03/basic/university/Student.java
@@ -1,5 +1,11 @@
 package com.farhad.example.design_principles03.basic.university;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
 
+    private String name;
 }
