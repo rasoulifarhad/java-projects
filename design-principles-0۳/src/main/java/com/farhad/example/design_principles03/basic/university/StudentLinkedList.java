39626179f5e10e1db06dc91085278980bd0a9975
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-20 00:09:52 +0330

    updated

--- design-principles-0۳/src/main/java/com/farhad/example/design_principles03/basic/university/StudentLinkedList.java
+++ design-principles-0۳/src/main/java/com/farhad/example/design_principles03/basic/university/StudentLinkedList.java
@@ -14,4 +14,15 @@ public class StudentLinkedList implements StudentList {
     public void print() {
     }
 
+    public static void main(String[] args) {
+        
+        StudentList students;
+        students = new StudentLinkedList();
+        Student s1 = new Student("s1", "address1");
+        students.add(s1);
+        s1 = new Student("s2", "address2");
+        students.add(s1);
+        students.print();
+
+    }
 }
