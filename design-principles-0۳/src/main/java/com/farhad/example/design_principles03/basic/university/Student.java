0f3e6228810a1c9e746084e55fd7aa9597bf62b5
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-19 23:04:36 +0330

    updated

--- design-principles-0۳/src/main/java/com/farhad/example/design_principles03/basic/university/Student.java
+++ design-principles-0۳/src/main/java/com/farhad/example/design_principles03/basic/university/Student.java
@@ -3,9 +3,28 @@ package com.farhad.example.design_principles03.basic.university;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class Student {
 
+    @Getter
+    @Setter
     private String name;
+
+    // grade point average
+    // it changes when a student gets a grade for a course.
+    @Getter
+    private double gpa;
+
+    public void addCourse(Course newCourse) {
+
+    }
+
+    private void computeGPA() {
+
+    }
+
+    public void assignGrade(Course aCourse, char newGrade) {
+        computeGPA();
+    }
+
+
 }
