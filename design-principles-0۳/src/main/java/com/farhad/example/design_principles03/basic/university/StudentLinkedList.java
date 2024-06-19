41106fd4b6ec8d4eb54cf8291017587889d588de
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-20 00:06:49 +0330

    updated

---
+++ design-principles-0۳/src/main/java/com/farhad/example/design_principles03/basic/university/StudentLinkedList.java
@@ -0,0 +1,17 @@
+package com.farhad.example.design_principles03.basic.university;
+
+public class StudentLinkedList implements StudentList {
+
+    @Override
+    public void add(Student student) {
+    }
+
+    @Override
+    public void delete(Student student) {
+    }
+
+    @Override
+    public void print() {
+    }
+
+}
