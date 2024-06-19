a6cca6247e5d546e1d4e5649c801ba8b4d2df9b1
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-19 23:12:22 +0330

    updated

--- design-principles-0۳/src/main/java/com/farhad/example/design_principles03/basic/university/Student.java
+++ design-principles-0۳/src/main/java/com/farhad/example/design_principles03/basic/university/Student.java
@@ -8,7 +8,14 @@ public class Student {
     @Getter
     @Setter
     private String name;
+    
+    @Getter
+    @Setter
+    private String address;
 
+    public Student(String studentName) {
+        this.name = studentName;
+    }   
     // grade point average
     // it changes when a student gets a grade for a course.
     @Getter
@@ -35,6 +42,9 @@ public class Student {
 
         // The implied meaning is that the student has completed the 
         // course (aCourse) with a grade of ’B’.
+        // compute the new GPA for the student using the information presumably 
+        // in the course (such as number of credits) and the number of points for 
+        // a grade of ‘B’.
         aStudent.assignGrade(aCourse, 'B');
         System.out.println(aStudent.getGpa());
     }
