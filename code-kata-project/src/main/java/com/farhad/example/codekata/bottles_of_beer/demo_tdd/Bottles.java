1695cd2954a3436bf0c72b01e87bd3230d9bd1f9
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-21 03:13:06 +0330

    updated

---
+++ code-kata-project/src/main/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/Bottles.java
@@ -0,0 +1,15 @@
+package com.farhad.example.codekata.bottles_of_beer.demo_tdd;
+
+public class Bottles {
+
+    public String verse(int i) {
+        return (
+            "99 bottles of beer on the wall, " +
+            "99 bottles of beer.\n" +
+            "Take one down and pass it around, " +
+            "98 bottles of beer on the wall.\n"
+        );
+    }
+    
+
+}
---
+++ code-kata-project/src/test/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/BottlesTest.java
@@ -0,0 +1,21 @@
+package com.farhad.example.codekata.bottles_of_beer.demo_tdd;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class BottlesTest {
+
+    @Test
+    public void theFirstVerse() {
+        String expected = 
+            "99 bottles of beer on the wall, " +
+            "99 bottles of beer.\n" +
+            "Take one down and pass it around, " +
+            "98 bottles of beer on the wall.\n";
+        assertThat(expected, is(new Bottles().verse(99)));
+    }
+
+}
