983ac867a127ab58ab4db5d5fd332b49909c50d1
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-21 03:26:25 +0330

    updated

--- code-kata-project/src/main/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/Bottles.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/Bottles.java
@@ -3,20 +3,23 @@ package com.farhad.example.codekata.bottles_of_beer.demo_tdd;
 public class Bottles {
 
     public String verse(int number) {
+        int n;
         if(number == 99) {
+            n = 99;
             return (
-                "99 bottles of beer on the wall, " +
-                "99 bottles of beer.\n" +
+                n + " bottles of beer on the wall, " +
+                n + " bottles of beer.\n" +
                 "Take one down and pass it around, " +
-                "98 bottles of beer on the wall.\n"
+                (n-1) + " bottles of beer on the wall.\n"
             );
         }
         if(number == 3) {
+            n = 3;
             return (
-                "3 bottles of beer on the wall, " +
-                "3 bottles of beer.\n" +
+                n + " bottles of beer on the wall, " +
+                n + " bottles of beer.\n" +
                 "Take one down and pass it around, " +
-                "2 bottles of beer on the wall.\n"
+                (n-1) + " bottles of beer on the wall.\n"
             );
         }
         return "";
