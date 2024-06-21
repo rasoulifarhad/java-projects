6fbb22bdb71eccaecdf3a7e076a34c0a1da67b2a
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-21 03:53:25 +0330

    updated

--- code-kata-project/src/main/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/Bottles.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/Bottles.java
@@ -3,11 +3,19 @@ package com.farhad.example.codekata.bottles_of_beer.demo_tdd;
 public class Bottles {
 
     public String verse(int number) {
+        if(number == 2) {
+            return (
+                number + " bottles of beer on the wall, " +
+                number + " bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                (number-1) + " bottle of beer on the wall.\n"
+            );
+        }
         return (
             number + " bottles of beer on the wall, " +
             number + " bottles of beer.\n" +
             "Take one down and pass it around, " +
-            (number-1) + " bottle" + ((number-1) == 1 ? "" : "s") + " of beer on the wall.\n"
+            (number-1) + " bottles of beer on the wall.\n"
         );
     }
     
