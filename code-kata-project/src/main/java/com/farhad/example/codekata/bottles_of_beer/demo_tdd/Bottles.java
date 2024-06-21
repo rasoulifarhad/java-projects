c0ae61a4fc088b6078563835dde1b4e37b7d7ef6
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-22 00:12:19 +0330

    updated

--- code-kata-project/src/main/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/Bottles.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/Bottles.java
@@ -59,12 +59,12 @@ public class Bottles {
                     number + " bottles of beer on the wall, " +
                     number + " bottles of beer.\n" +
                     "Take one down and pass it around, " +
-                    (number - 1) + " " + container() + " of beer on the wall.\n"
+                    (number - 1) + " " + container(number-1) + " of beer on the wall.\n"
                 );
         }
     }
 
-    String container() {
+    String container(int number) {
         return "bottles";
     }
 }
