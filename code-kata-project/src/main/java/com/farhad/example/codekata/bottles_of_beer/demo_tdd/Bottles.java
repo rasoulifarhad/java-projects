085a68367681a3d9f42154ae74066d17887f9d60
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-21 04:08:12 +0330

    uupdated

--- code-kata-project/src/main/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/Bottles.java
+++ code-kata-project/src/main/java/com/farhad/example/codekata/bottles_of_beer/demo_tdd/Bottles.java
@@ -3,28 +3,29 @@ package com.farhad.example.codekata.bottles_of_beer.demo_tdd;
 public class Bottles {
 
     public String verse(int number) {
-        if(number == 1) {
-            return (
-                "1 bottle of beer on the wall, " +
-                "1 bottle of beer.\n" +
-                "Take it down and pass it around, " +
-                "no more bottles of beer on the wall.\n"
-            );
+        switch (number) {
+            case 1:
+                return (
+                    "1 bottle of beer on the wall, " +
+                    "1 bottle of beer.\n" +
+                    "Take it down and pass it around, " +
+                    "no more bottles of beer on the wall.\n"
+                );
+            case 2:
+                return (
+                    "2 bottles of beer on the wall, " +
+                    "2 bottles of beer.\n" +
+                    "Take one down and pass it around, " +
+                    "1 bottle of beer on the wall.\n"
+                );
+            default:
+                return (
+                    number + " bottles of beer on the wall, " +
+                    number + " bottles of beer.\n" +
+                    "Take one down and pass it around, " +
+                    (number - 1) + " bottles of beer on the wall.\n"
+                );
         }
-        if(number == 2) {
-            return (
-                "2 bottles of beer on the wall, " +
-                "2 bottles of beer.\n" +
-                "Take one down and pass it around, " +
-                "1 bottle of beer on the wall.\n"
-            );
-        }
-        return (
-            number + " bottles of beer on the wall, " +
-            number + " bottles of beer.\n" +
-            "Take one down and pass it around, " +
-            (number-1) + " bottles of beer on the wall.\n"
-        );
     }
     
 
