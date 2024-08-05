cac25ae3820bfb620a77738701ef1b58c2117f3a
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-05 23:49:21 +0330

    updated

--- simple-java-project/src/main/java/com/farhad/example/work_effevtively_with_legacy_code/fake_objects/Iiritating_parameter/RGHConnection.java
+++ simple-java-project/src/main/java/com/farhad/example/work_effevtively_with_legacy_code/fake_objects/Iiritating_parameter/RGHConnection.java
@@ -1,5 +1,18 @@
 package com.farhad.example.work_effevtively_with_legacy_code.fake_objects.Iiritating_parameter;
 
+import java.io.IOException;
+
+// When an RGHConnection is constructed, it connects with a server. The connection 
+// uses that server to get all of the reports it needs to validate a customer’s credit.
 public class RGHConnection {
 
+    private int port;
+    private String name;
+    private String passwd;
+
+    public RGHConnection(int port, String name, String passwd) throws IOException {
+        this.port = port;
+        this.name = name;
+        this.passwd = passwd;
+    }
 }
