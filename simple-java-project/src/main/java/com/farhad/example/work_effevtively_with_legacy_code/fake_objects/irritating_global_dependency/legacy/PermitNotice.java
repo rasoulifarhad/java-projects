73e0f925abb754ee61a2fcc32eb1b1b122e36852
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-08 22:31:34 +0330

    updated

---
+++ simple-java-project/src/main/java/com/farhad/example/work_effevtively_with_legacy_code/fake_objects/irritating_global_dependency/legacy/PermitNotice.java
@@ -0,0 +1,13 @@
+package com.farhad.example.work_effevtively_with_legacy_code.fake_objects.irritating_global_dependency.legacy;
+
+public class PermitNotice {
+
+    public PermitNotice(int i, String string) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public boolean isValid() {
+        return false;
+    }
+
+}
---
+++ simple-java-project/src/test/java/com/farhad/example/work_effevtively_with_legacy_code/fake_objects/irritating_global_dependency/legacy/FacilityTest.java
@@ -0,0 +1,12 @@
+package com.farhad.example.work_effevtively_with_legacy_code.fake_objects.irritating_global_dependency.legacy;
+
+import org.junit.jupiter.api.Test;
+
+public class FacilityTest {
+
+    @Test
+    public void testCreate() throws PermitViolation {
+        PermitNotice notice = new PermitNotice(0, "a");
+        Facility facility = new Facility(Facility.RESIDENCE, "b", notice);
+    }
+}
