fd1969f3beb30fd579955731bc8c7183fc55b38e
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-08 23:52:10 +0330

    updated

--- simple-java-project/src/test/java/com/farhad/example/work_effevtively_with_legacy_code/fake_objects/irritating_global_dependency/better/FacilityTest.java
+++ simple-java-project/src/test/java/com/farhad/example/work_effevtively_with_legacy_code/fake_objects/irritating_global_dependency/better/FacilityTest.java
@@ -1,8 +1,19 @@
 package com.farhad.example.work_effevtively_with_legacy_code.fake_objects.irritating_global_dependency.better;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FacilityTest {
+
+    @BeforeEach
+    public void setup() {
+        PermitRepository repository = PermitRepository.getInstance();
+        //
+        // add permits to repository here
+        //
+
+        PermitRepository.setTestingInstance(repository);
+    }
 
     @Test
     public void testCreate() throws PermitViolation {
