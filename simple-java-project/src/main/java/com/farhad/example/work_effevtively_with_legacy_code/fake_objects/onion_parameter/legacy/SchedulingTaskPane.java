19a35aac193e3bf2b9e96669ee7818fa2c2ff881
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-10 01:03:18 +0330

    updated

---
+++ simple-java-project/src/main/java/com/farhad/example/work_effevtively_with_legacy_code/fake_objects/onion_parameter/legacy/SchedulingTaskPane.java
@@ -0,0 +1,10 @@
+package com.farhad.example.work_effevtively_with_legacy_code.fake_objects.onion_parameter.legacy;
+
+public class SchedulingTaskPane extends SchedulerPane {
+
+    private SchedulingTask task;
+
+    public SchedulingTaskPane(SchedulingTask task) {
+        this.task = task;
+    }
+}
