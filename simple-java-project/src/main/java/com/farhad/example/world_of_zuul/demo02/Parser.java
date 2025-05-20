f3b8ea4de171370b9abdd59c789b4e0a57efc812
farhad rasouli <rasouli.farhad@gmail.com>
2025-05-20 16:56:43 +0330

    updated

---
+++ simple-java-project/src/main/java/com/farhad/example/world_of_zuul/demo02/Parser.java
@@ -0,0 +1,39 @@
+package com.farhad.example.world_of_zuul.demo02;
+
+import java.util.Scanner;
+
+public class Parser {
+
+    private CommandWords commandWords;
+    private Scanner reader;
+
+    public Parser() {
+        commandWords = new CommandWords();
+        reader = new Scanner(System.in);
+    }
+
+    public Command getCommand() {
+        String inputLine;
+        String word1 = null;
+        String word2 = null;
+
+        System.out.println("> ");
+
+        inputLine = reader.nextLine();
+
+        Scanner tokenizer = new Scanner(inputLine);
+        if (tokenizer.hasNext()) {
+            word1 = tokenizer.next();
+            if (tokenizer.hasNext()) {
+                word2 = tokenizer.next();
+            }
+        }
+
+        if (commandWords.isCommand(word1)) {
+            return new Command(word1, word2);
+        } else {
+            return new Command(null, word2);
+        }
+    }
+
+}
