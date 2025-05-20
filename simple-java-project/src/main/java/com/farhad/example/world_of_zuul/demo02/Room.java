ebfa83d19a0cd693b75118c3af3f082f7c9db84c
farhad rasouli <rasouli.farhad@gmail.com>
2025-05-20 17:23:18 +0330

    updated

--- simple-java-project/src/main/java/com/farhad/example/world_of_zuul/demo02/Room.java
+++ simple-java-project/src/main/java/com/farhad/example/world_of_zuul/demo02/Room.java
@@ -1,29 +1,30 @@
 package com.farhad.example.world_of_zuul.demo02;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Room {
 
     private String description;
-    private Room northExit;
-    private Room southExit;
-    private Room eastExit;
-    private Room westExit;
+    private Map<String, Room> exits;
 
     public Room(String description) {
         this.description = description;
+        this.exits = new HashMap<>();
     }
 
     public void setExits(Room northExit, Room southExit, Room eastExit, Room westExit) {
         if (northExit != null) {
-            this.northExit = northExit;
+            exits.put("north", northExit);
         }
         if (southExit != null) {
-            this.southExit = southExit;
+            exits.put("south", southExit);
         }
         if (eastExit != null) {
-            this.eastExit = eastExit;
+            exits.put("east", eastExit);
         }
         if (westExit != null) {
-            this.westExit = westExit;
+            exits.put("west", westExit);
         }
     }
 
@@ -32,18 +33,6 @@ public class Room {
     }
 
     public Room getExit(String direction) {
-        if (direction.equals("north")) {
-            return northExit;
-        }
-        if (direction.equals("east")) {
-            return eastExit;
-        }
-        if (direction.equals("south")) {
-            return southExit;
-        }
-        if (direction.equals("west")) {
-            return westExit;
-        }
-        return null;
+        return exits.get(direction);
     }
 }
